1f82be46d99de885bd30edc095905dbc1dba47a9
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-05-31 09:32:29 +0200

    Remove functions comments

--- implementation/contracts/interfaces/KeepBridge.sol
+++ implementation/contracts/interfaces/KeepBridge.sol
@@ -10,23 +10,17 @@ contract KeepBridge is IKeep {
         keepRegistry = _keepRegistry;
     }
 
-    // returns the timestamp when it was approved
     function wasDigestApprovedForSigning(uint256 _keepID, bytes32 _digest) external view returns (uint256){
         //TODO: Implement
         return 0;
     }
 
-    // onlyKeepOwner
-    // record a digest as approved for signing
+
     function approveDigest(uint256 _keepID, bytes32 _digest) external returns (bool _success){
         //TODO: Implement
         return _success;
     }
 
-    // Expected behavior:
-    // Error if not fraud
-    // Return true if fraud
-    //     This means if the signature is valid, but was not approved via approveDigest
     function submitSignatureFraud(
         uint256 _keepID,
         uint8 _v,
@@ -39,23 +33,16 @@ contract KeepBridge is IKeep {
         return _isFraud;
     }
 
-    // Allow sending funds to a keep group
-    // Expected: increment their existing ETH bond
     function distributeEthToKeepGroup(uint256 _keepID) external payable returns (bool){
         //TODO: Implement
         return false;
     }
 
-    // Allow sending tokens to a keep group
-    // Useful for sending signers their TBTC
-    // The Keep contract should call transferFrom on the token contrcact
     function distributeERC20ToKeepGroup(uint256 _keepID, address _asset, uint256 _value) external returns (bool){
         //TODO: Implement
         return false;
     }
 
-    // request a new m-of-n group
-    // should return a 256 unique keep id
     function requestKeepGroup(uint256 _m, uint256 _n) external payable returns (uint256 _keepID){
         //TODO: Implement
 
@@ -81,9 +68,6 @@ contract KeepBridge is IKeep {
         return 0;
     }
 
-    // seize the signer's ETH bond
-    // onlyKeepOwner
-    // msg.sender.transfer(bondAmount)
     function seizeSignerBonds(uint256 _keepID) external returns (bool){
         //TODO: Implement
         return false;
