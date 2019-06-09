b1489aa464bc2659ed0df8cf9a5d2361dc590268
liamzebedee <liamzebedee@yahoo.com.au>
2019-06-09 21:19:36 +0200

    Add README and Seth deploy script

---
+++ implementation/uniswap/README.md
@@ -0,0 +1,4 @@
+uniswap
+=======
+
+A vendored deployment of the Uniswap contracts, for private testnets. It uses [Seth](https://github.com/dapphub/dapptools/tree/master/src/seth).
---
+++ implementation/uniswap/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ex
+
+# Connect to the local node
+ETH_RPC_URL=http://127.0.0.1:8545
+
+# Use the unlocked RPC accounts
+ETH_RPC_ACCOUNTS=yes
+
+# Get the 1st account and use it
+ETH_FROM=$(seth accounts | head -n1 | awk '{ print $1 }')
+
+# 1/ Deploy Exchange
+EXCHANGE_ADDR=$(seth send --create $(cat ./contracts-vyper/bytecode/exchange.txt) --status 2>/dev/null)
+
+# 2/ Deploy Factory
+FACTORY_ADDR=$(seth send --create $(cat ./contracts-vyper/bytecode/factory.txt) --status 2>/dev/null)
+
+# 3/ 
+seth send $FACTORY_ADDR "initializeFactory(address)" $EXCHANGE_ADDR
+
+
+rm deployments.txt
+echo "Exchange $EXCHANGE_ADDR" >> deployments.txt
+echo "Factory $FACTORY_ADDR" >> deployments.txt
