bccc6bd52ad5c15c43025fc6fb39f289eeb8f39c
Nicholas Evans <nicholas.evans@thesis.co>
2019-06-19 05:42:06 -0500

    Further script implementation

--- implementation/demo/create_new_deposit.js
+++ implementation/demo/create_new_deposit.js
@@ -1,8 +1,9 @@
-var Deposit = artifacts.require("./Deposit.sol");
-var KeepMediator = artifacts.require("./KeepBridge.sol");
+var Deposit        = artifacts.require("./Deposit.sol");
+var KeepMediator   = artifacts.require("./KeepBridge.sol");
 
-const keepRegistry = "0xc84Ba13B9a6f3506557dF7A78A9a63d87e1f1FcC"; // KeepRegistry contract address
-const TBTCSystem = "0x3790354Be419094B6af6c80b1dB6521c82B75192"; //TBTC System contract address
+const keepRegistry = "0xdF534b280A95B614F18E9e2204f2599AB6fba84B"; // KeepRegistry contract address
+const TBTCSystem   = "0x39B2F1020e1692A495261D89a58624946DcE825e"; //TBTC System contract address
+const TBTCToken    = "0xc4Ba466Cf38B21AE7cCd9D10d546D17485dD50Ca"; //TBTC TOKEN
 
 module.exports = async function () {
     let deposit = await Deposit.deployed();
@@ -22,7 +23,7 @@ module.exports = async function () {
         console.log('Call createNewDeposit');
         let result = await deposit.createNewDeposit(
             TBTCSystem, // address _TBTCSystem,
-            "0x0000000000000000000000000000000000000000", // address _TBTCToken,
+            TBTCToken, // address _TBTCToken,
             keepBridge.address, // address _KeepBridge,
             5, // uint256 _m,
             10 // uint256 _n
