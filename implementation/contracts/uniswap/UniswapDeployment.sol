31207ded8282bec8c604653b0e943ff1f8c6dd59
liamzebedee <liamzebedee@yahoo.com.au>
2019-06-26 19:31:58 +0200

    Doc UniswapDeployment

--- implementation/contracts/uniswap/UniswapDeployment.sol
+++ implementation/contracts/uniswap/UniswapDeployment.sol
@@ -1,5 +1,7 @@
 pragma solidity 0.4.25;
 
+/// @notice A contract for tracking the location of Uniswap testnet deployments
+/// @dev useful for usage in tests/migrations, but SHOULD NOT be used by other contracts
 contract UniswapDeployment {
     address public factory;
     address public exchange;
