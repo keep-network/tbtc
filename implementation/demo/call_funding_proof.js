756a9c2c43ebe7ef12c516f397e9fbaf8bb226a5
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-06-26 14:13:27 +0200

    Log Funded event in demo script

--- implementation/demo/call_funding_proof.js
+++ implementation/demo/call_funding_proof.js
@@ -1,24 +1,30 @@
 let Deposit = artifacts.require("./Deposit.sol");
+let TBTCSystem = artifacts.require("./TBTCSystemStub.sol");
 
 const FundingProof = require('./tools/FundingProof')
 
 module.exports = async function () {
     let deposit
+    let depositLog
 
     let txID = process.argv[4]
     let headerLen = process.argv[5]
     let fundingOutputIndex = process.argv[6]
 
     async function initContracts() {
-        deposit = await Deposit.deployed().catch((err) => {
+        try {
+            deposit = await Deposit.deployed()
+            depositLog = await TBTCSystem.deployed();
+        } catch (err) {
             console.error(`call failed: ${err}`)
             process.exit(1)
-        });
+        }
     }
 
     async function callFundingProof(fundingProof) {
         console.log('Submit funding proof...');
-        console.log(fundingProof);
+
+        let blockNumber = await web3.eth.getBlock("latest").number
 
         let result = await deposit.provideBTCFundingProof(
             fundingProof.version,
@@ -30,10 +36,17 @@ module.exports = async function () {
             fundingProof.txInBlockIndex,
             fundingProof.chainHeaders
         ).catch((err) => {
-            console.error(`call failed: ${err}`)
+            console.error(`provideBTCFundingProof failed: ${err}`)
         });
 
-        console.log('funding call tx: ' + result.tx)
+        console.log("provideBTCFundingProof transaction: ", result.tx)
+
+        let eventList = await depositLog.getPastEvents('Funded', {
+            fromBlock: blockNumber,
+            toBlock: 'latest'
+        })
+
+        console.log("Funding proof accepted for the deposit: ", eventList[0].returnValues._depositContractAddress)
     }
 
     await initContracts()
