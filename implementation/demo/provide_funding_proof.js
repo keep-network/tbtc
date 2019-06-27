ac66d8252379573114b86634851b361d55ba2ef0
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-06-27 10:33:29 +0200

    Linting fix

--- implementation/demo/provide_funding_proof.js
+++ implementation/demo/provide_funding_proof.js
@@ -1,53 +1,53 @@
-let Deposit = artifacts.require("./Deposit.sol");
-let TBTCSystem = artifacts.require("./TBTCSystemStub.sol");
+const Deposit = artifacts.require('./Deposit.sol')
+const TBTCSystem = artifacts.require('./TBTCSystemStub.sol')
 
 const FundingProof = require('./tools/FundingProof')
 
-module.exports = async function () {
-    let deposit
-    let depositLog
+module.exports = async function() {
+  let deposit
+  let depositLog
 
-    let txID = process.argv[4]
-    let headerLen = process.argv[5]
+  const txID = process.argv[4]
+  const headerLen = process.argv[5]
 
-    async function initContracts() {
-        try {
-            deposit = await Deposit.deployed()
-            depositLog = await TBTCSystem.deployed();
-        } catch (err) {
-            console.error(`call failed: ${err}`)
-            process.exit(1)
-        }
+  async function initContracts() {
+    try {
+      deposit = await Deposit.deployed()
+      depositLog = await TBTCSystem.deployed()
+    } catch (err) {
+      console.error(`call failed: ${err}`)
+      process.exit(1)
     }
+  }
 
-    async function callFundingProof(fundingProof) {
-        console.log('Submit funding proof...');
+  async function callFundingProof(fundingProof) {
+    console.log('Submit funding proof...')
 
-        let blockNumber = await web3.eth.getBlock("latest").number
+    const blockNumber = await web3.eth.getBlock('latest').number
 
-        let result = await deposit.provideBTCFundingProof(
-            fundingProof.version,
-            fundingProof.txInVector,
-            fundingProof.txOutVector,
-            fundingProof.locktime,
-            fundingProof.fundingOutputIndex,
-            fundingProof.merkleProof,
-            fundingProof.txInBlockIndex,
-            fundingProof.chainHeaders
-        ).catch((err) => {
-            console.error(`provideBTCFundingProof failed: ${err}`)
-        });
+    const result = await deposit.provideBTCFundingProof(
+      fundingProof.version,
+      fundingProof.txInVector,
+      fundingProof.txOutVector,
+      fundingProof.locktime,
+      fundingProof.fundingOutputIndex,
+      fundingProof.merkleProof,
+      fundingProof.txInBlockIndex,
+      fundingProof.chainHeaders
+    ).catch((err) => {
+      console.error(`provideBTCFundingProof failed: ${err}`)
+    })
 
-        console.log("provideBTCFundingProof transaction: ", result.tx)
+    console.log('provideBTCFundingProof transaction: ', result.tx)
 
-        let eventList = await depositLog.getPastEvents('Funded', {
-            fromBlock: blockNumber,
-            toBlock: 'latest'
-        })
+    const eventList = await depositLog.getPastEvents('Funded', {
+      fromBlock: blockNumber,
+      toBlock: 'latest',
+    })
 
-        console.log("Funding proof accepted for the deposit: ", eventList[0].returnValues._depositContractAddress)
-    }
+    console.log('Funding proof accepted for the deposit: ', eventList[0].returnValues._depositContractAddress)
+  }
 
-    await initContracts()
-    await FundingProof.getTransactionProof(txID, headerLen, callFundingProof)
+  await initContracts()
+  await FundingProof.getTransactionProof(txID, headerLen, callFundingProof)
 }
