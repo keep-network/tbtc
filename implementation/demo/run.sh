2a774ce0a6a3a96cd84971220bae140917d529e3
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-07-05 12:39:06 +0200

    Update run.sh to handle deposit address
    Deposit address will be taken from the first script output and passed to
    subsequent steps.

--- implementation/demo/run.sh
+++ implementation/demo/run.sh
@@ -2,14 +2,17 @@
 set -e
 
 echo '--- CREATE NEW DEPOSIT'
-truffle exec 1_create_new_deposit.js
+
+output=$(truffle exec 1_create_new_deposit.js)
+echo "$output"
+depositAddress=$(echo "$output" | awk '/new deposit deployed:/ { print $4 }')
 
 echo '--- FETCH PUBLIC KEY'
 
-truffle exec 2_fetch_public_key.js
+truffle exec 2_fetch_public_key.js $depositAddress
 
 echo '--- SUBMIT FUNDING PROOF'
 echo "Enter funding transaction ID and press [ENTER]: "
 read txID
 
-truffle exec 3_provide_funding_proof.js $txID 6 0
+truffle exec 3_provide_funding_proof.js $depositAddress $txID 6
