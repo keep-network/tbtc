44e2cb96f2df3f9c2651669d9db25101060158df
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-07-05 12:33:05 +0200

    Move deposit deployment inside function

--- implementation/demo/1_create_new_deposit.js
+++ implementation/demo/1_create_new_deposit.js
@@ -10,17 +10,17 @@ module.exports = async function() {
   try {
     keepBridge = await KeepBridge.deployed()
     tbtcSystem = await TBTCSystem.deployed()
-
-    // This is a temporary solution to deploy a new contract. It will be replaced
-    // by a deposit factory.
-    deposit = await Deposit.new()
-    console.log('new deposit deployed: ', deposit.address)
   } catch (err) {
     console.error(`initialization failed: ${err}`)
     process.exit(1)
   }
 
   async function createNewDeposit() {
+    // This is a temporary solution to deploy a new contract. It will be replaced
+    // by a deposit factory.
+    deposit = await Deposit.new()
+    console.log('new deposit deployed: ', deposit.address)
+
     const result = await deposit.createNewDeposit(
       tbtcSystem.address, // address _TBTCSystem,
       '0x0000000000000000000000000000000000000000', // address _TBTCToken,
