4e5534fde6281915c3e10c93975030d781575d1b
liamzebedee <liamzebedee@yahoo.com.au>
2019-07-10 22:29:43 +0200

    NatSpec for IPriceOracle

--- implementation/contracts/interfaces/IPriceOracle.sol
+++ implementation/contracts/interfaces/IPriceOracle.sol
@@ -1,16 +1,13 @@
 pragma solidity 0.4.25;
 
-/**
- * @title Bitcoin-Ether price oracle interface.
- */
-
+/// @title Bitcoin-Ether price oracle interface.
 contract IPriceOracle {
     event PriceUpdated(uint128 price, uint256 expiry);
 
-    // Get the current price of one satoshi in wei
-    // reverts if the price is invalid due to expiry
+    /// @notice Get the current price of one satoshi in wei
+    /// @dev reverts if the price is invalid due to expiry
     function getPrice() external view returns (uint128);
 
-    // Updates the price in terms of x wei : 1 satoshi
+    /// @notice Updates the price in terms of x wei : 1 satoshi
     function updatePrice(uint128 x) external;
 }
