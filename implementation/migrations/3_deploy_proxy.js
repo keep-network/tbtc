2763ec3f71a09f41559961dbbeb0246d8d37df97
Nicholas Evans <nicholas.evans@thesis.co>
2019-07-11 04:36:35 -0500

    Add migration file for Deposit factory

---
+++ implementation/migrations/3_deploy_proxy.js
@@ -0,0 +1,12 @@
+// proxy
+// const CloneFactory = artifacts.require(CloneFactory)
+const DepositFactory = artifacts.require('DepositFactory')
+const Deposit = artifacts.require('Deposit')
+
+module.exports = (deployer) => {
+  deployer.then(async () => {
+    // proxy
+    const deposit = await Deposit.deployed()
+    await deployer.deploy(DepositFactory, deposit.address)
+  })
+}
