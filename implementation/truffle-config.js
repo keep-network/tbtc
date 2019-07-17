a435fdaac68f23b951e871fa7dd79898b48cdc26
liamzebedee <liamzebedee@yahoo.com.au>
2019-07-17 14:37:32 +0200

    Add coverage network env

--- implementation/truffle-config.js
+++ implementation/truffle-config.js
@@ -52,6 +52,14 @@ module.exports = {
      gasPrice: 1
     },
 
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+    },
+
     // Another network with more advanced options...
     // advanced: {
       // port: 8777,             // Custom port
