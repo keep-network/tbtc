36ac1de81eeb076eb7fb7d3e6fb094a7f39e468a
Nicholas Evans <nicholas.evans@thesis.co>
2019-07-17 06:03:49 -0500

    Fix solidity lint errors

--- implementation/contracts/proxy/DepositFactory.sol
+++ implementation/contracts/proxy/DepositFactory.sol
@@ -4,14 +4,14 @@ import "@optionality.io/clone-factory/contracts/CloneFactory.sol";
 import "../deposit/Deposit.sol";
 
 /// @title Deposit Factory
-/// @notice Factory for the creation of new deposit clones. 
+/// @notice Factory for the creation of new deposit clones.
 /// @dev We avoid redeployment of deposit contract by using the clone factory.
 /// Proxy delegates calls to Deposit and therefore does not affect deposit state.
 /// This means that we only need to deploy the deposit contracts once.
 /// The factory provides clean state for every new deposit clone.
 contract DepositFactory is CloneFactory{
 
-    // Holds the address of the deposit contract 
+    // Holds the address of the deposit contract
     // which will be used as a master contract for cloning.
     address public masterDepositAddress;
 
@@ -25,7 +25,7 @@ contract DepositFactory is CloneFactory{
     }
 
     /// @notice                Creates a new deposit instance
-    /// @dev                   Calls createNewDeposit from deposit contract as init method. 
+    /// @dev                   Calls createNewDeposit from deposit contract as init method.
     ///                        We don't offer pure createClone, meaning that the only way
     ///                        to create a clone is by also calling createNewDeposit()
     ///                        Deposits created this way will never pass by state 0 (START)
