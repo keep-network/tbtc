fa44986c699204fdd0a174144a0f2c69c20d4649
Jakub Nowakowski <jakub.nowakowski@akena.co>
2019-07-25 08:17:37 +0200

    Fix indent

--- implementation/contracts/interfaces/IKeep.sol
+++ implementation/contracts/interfaces/IKeep.sol
@@ -10,7 +10,7 @@ interface IKeep {
     // should return a unique keep contract address
     function requestNewKeep(uint256 _m, uint256 _n) external payable returns (address _keepAddress);
 
-        // get the result of a keep formation
+    // get the result of a keep formation
     // should return a 64 byte packed pubkey (x and y)
     // error if not ready yet
     function getKeepPubkey(address _keepAddress) external view returns (bytes memory);
