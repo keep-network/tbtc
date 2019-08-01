9185b8d198b07a13b34ed2283700d8f63d10e10e
Nicholas Evans <nicholas.evans@thesis.co>
2019-08-01 07:04:35 -0500

    Fix TBTCSystem comment style

--- implementation/test/contracts/deposit/TBTCSystemStub.sol
+++ implementation/test/contracts/deposit/TBTCSystemStub.sol
@@ -4,11 +4,20 @@ import {TBTCSystem} from '../../../contracts/system/TBTCSystem.sol';
 
 contract TBTCSystemStub is TBTCSystem {
 
-    function setOraclePrice(uint256 _oraclePrice) external {oraclePrice = _oraclePrice;}
-    function setCurrentDiff(uint256 _currentDifficulty) external {currentDifficulty = _currentDifficulty;}
-    function setPreviousDiff(uint256 _previousDifficulty) external {previousDifficulty = _previousDifficulty;}
-    //function setDepositOwner(address _keepAddress, address _newOwner) external {_keepAddress; depositOwner = _newOwner;}
+    function setOraclePrice(uint256 _oraclePrice) external {
+        oraclePrice = _oraclePrice;
+    }
+
+    function setCurrentDiff(uint256 _currentDifficulty) external {
+        currentDifficulty = _currentDifficulty;
+    }
+
+    function setPreviousDiff(uint256 _previousDifficulty) external {
+        previousDifficulty = _previousDifficulty;
+    }
 
     // override parent
-    function approvedToLog(address _caller) public view returns (bool) {_caller; return true;}
+    function approvedToLog(address _caller) public view returns (bool) {
+        _caller; return true;
+    }
 }
