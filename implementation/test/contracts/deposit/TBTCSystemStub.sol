48d2cd292e912966c7a98bff6634e748134ef3c8
Nicholas Evans <nicholas.evans@thesis.co>
2019-08-20 15:28:09 +0300

    Update TBTCSystem Stub
    Implemebt System functions to call token contract
    
    Remove clearBalance function to move towards using the real System contract in testing

--- implementation/test/contracts/deposit/TBTCSystemStub.sol
+++ implementation/test/contracts/deposit/TBTCSystemStub.sol
@@ -1,8 +1,15 @@
 pragma solidity ^0.5.10;
 
 import {TBTCSystem} from '../../../contracts/system/TBTCSystem.sol';
+import {TBTCToken} from "../../../contracts/system/TBTCToken.sol";
 
 contract TBTCSystemStub is TBTCSystem {
+
+    address _TBTCToken;
+
+    function setExternalAddresses(address _tokenAddress) public {
+        _TBTCToken = _tokenAddress;
+    }
 
     function setOraclePrice(uint256 _oraclePrice) external {
         oraclePrice = _oraclePrice;
@@ -19,5 +26,18 @@ contract TBTCSystemStub is TBTCSystem {
     // override parent
     function approvedToLog(address _caller) public view returns (bool) {
         _caller; return true;
+    }
+
+    //Token functions
+    function systemMint(address _account, uint256 _amount) public returns (bool){
+        return TBTCToken(_TBTCToken).mint(_account, _amount);
+    }
+
+    function systemBurnFrom(address _account, uint256 _amount) public {
+        TBTCToken(_TBTCToken).burnFrom(_account, _amount);
+    }
+
+    function systemTransferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        return TBTCToken(_TBTCToken).transferFrom(_from, _to, _value);
     }
 }
