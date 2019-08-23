89be87e5c4bedcedd1787a5aae981233b9e8c591
Nicholas Evans <nicholas.evans@thesis.co>
2019-08-23 19:08:52 +0300

    Update TokenAddress naming in Redemption

--- implementation/contracts/deposit/DepositRedemption.sol
+++ implementation/contracts/deposit/DepositRedemption.sol
@@ -30,13 +30,13 @@ library DepositRedemption {
     /// @notice     Pushes signer fee to the Keep group by transferring it to the Keep address
     /// @dev        Approves the keep contract, then expects it to call transferFrom
     function distributeSignerFee(DepositUtils.Deposit storage _d) public {
-        address _tbtcAddress = _d.TBTCToken;
-        TBTCToken _token = TBTCToken(_tbtcAddress);
+        address _tbtcTokenAddress = _d.TBTCToken;
+        TBTCToken _token = TBTCToken(_tbtcTokenAddress);
 
         IKeep _keep = IKeep(_d.KeepBridge);
 
         _token.approve(_d.KeepBridge, DepositUtils.signerFee());
-        _keep.distributeERC20ToKeepGroup(_d.keepAddress, _tbtcAddress, DepositUtils.signerFee());
+        _keep.distributeERC20ToKeepGroup(_d.keepAddress, _tbtcTokenAddress, DepositUtils.signerFee());
     }
 
     /// @notice         approves a digest for signing by our keep group
