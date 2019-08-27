e7cdbe1d32aa1729cee24e6117ee78d3e82ab924
liamzebedee <liamzebedee@yahoo.com.au>
2019-08-27 18:18:01 +0200

    Remove misc comments

--- implementation/test/UniswapDeploymentTest.js
+++ implementation/test/UniswapDeploymentTest.js
@@ -8,7 +8,7 @@ import { UniswapHelpers } from './helpers/uniswap'
 
 // Tests the Uniswap deployment
 
-contract.only('Uniswap', (accounts) => {
+contract('Uniswap', (accounts) => {
   let tbtcToken
 
   describe('deployment', async () => {
@@ -35,7 +35,6 @@ contract.only('Uniswap', (accounts) => {
       tbtc = await TBTCToken.new()
 
       // We rely on the already pre-deployed Uniswap factory here.
-      const tbtcSystem = await TBTCSystem.deployed()
       const uniswapDeployment = await UniswapDeployment.deployed()
       const uniswapFactoryAddr = await uniswapDeployment.factory()
 
@@ -122,151 +121,3 @@ contract.only('Uniswap', (accounts) => {
     })
   })
 })
-
-
-/**
-
-
-Some notes on the cryptoeconomics of Uniswap and TBTC, time arbitrage between chains
-
- 1. Deposit abort flow is started
- 2. Bond goes for automatic liquidation via Uniswap
- 3. We liquidate the signer bonds `150% * fetchOraclePrice()`. Potential options:
-  a. Uniswap 100% of ETH
-  b. Uniswap 50% ETH and 50% falling price auction
-
-I think it's worth documenting the additional complexity stemming from automatic liquidation. 3a won't necessarily buy up enough TBTC to burn, depending on the pool size, but the arbitrage incentives are going to keep the price stable to its price-oracle reported value (with a little delta).
-*/
-
-/**
- *
-
-source sethenv.sh
-
-# deploy uniswap
-export FACTORY=$(seth send --create $(cat uniswap/contracts-vyper/bytecode/factory.txt))
-export EXCHANGE_TMPL=$(seth send --create $(cat uniswap/contracts-vyper/bytecode/exchange.txt))
-seth send $FACTORY "initializeFactory(address)" $EXCHANGE_TMPL
-
-
-#export TBTC=$(jq -r '.networks["5777"].address' build/contracts/TBTC.json)
-export UNISWAP_DEPLOY=$(jq -r '.networks["5777"].address' build/contracts/UniswapDeployment.json)
-export FACTORY=$(seth call $UNISWAP_DEPLOY "factory()(address)")
-
-export EXCHANGE=$(seth call $FACTORY "getExchange(address)(address)" $TBTC)
-seth balance $EXCHANGE
-
-export BUYER=$(seth accounts | sed -n 2p | awk '{ print $1 }')
-seth balance $BUYER
-
-
-# mint some tbtc
-export TBTC=$(seth send --create $(cat build/contracts/TBTC.json | jq -r .bytecode))
-seth send $TBTC "mint(address,uint256)" $BUYER $(seth --to-uint256 10000000000)
-seth call $TBTC "balanceOf(address)(uint256)" $BUYER
-
-# create exchange + approve
-seth send $FACTORY "createExchange(address)(address)" $TBTC
-EXCHANGE=$(seth call $FACTORY "getExchange(address)(address)" $TBTC)
-seth send --from $BUYER $TBTC "approve(address,uint)(bool)" $EXCHANGE $(seth --to-uint256 10000000000)
-seth call $TBTC "allowance(address,address)(uint256)" $BUYER $EXCHANGE
-
-
-# add to exchange
-# value must be above 1000000000 wei
-export DEADLINE=$(bc <<< "$(perl -MTime::HiRes=time -e 'printf "%d\n", time') + 200000")
-seth send --from $BUYER --value 100 $EXCHANGE "addLiquidity(uint256,uint256,uint256)(uint256)" 1 "$(seth --to-uint256 100000000)" $DEADLINE
-
-seth call $TBTC "balanceOf(address)(uint256)" $EXCHANGE
-seth balance $EXCHANGE
-
-# now estimate a proper gas margin
-export ETH_SOLD=$(seth --to-uint256 500000)
-
-export PRICE=$(seth --to-dec $(seth call $EXCHANGE "getTokenToEthInputPrice(uint256)(uint256)" $ETH_SOLD))
-seth send --from $ETH_FROM --value $PRICE $EXCHANGE "ethToTokenSwapInput(uint256,uint256)" $BUY_AMT $DEADLINE
-
-
- */
-/**
- *
- *
- *
- *
-
-
-export ETH_FROM=$(seth accounts | head -n1 | awk '{ print $1 }')
-seth send --create $(cat uniswap/contracts-vyper/bytecode/factory.txt)
-# 0xf0d61329351932e6e83517e5d0cb666dacb9589d
-seth send --create $(cat uniswap/contracts-vyper/bytecode/exchange.txt)
-# 0x61af707a9179e171bfc6d6e27dca18c5ab64c577
-
-seth send 0xf0d61329351932e6e83517e5d0cb666dacb9589d "initializeFactory(address)" 0x61af707a9179e171bfc6d6e27dca18c5ab64c577
-
-
-seth send --create $(cat build/contracts/TBTC.json | jq -r .bytecode)
-export TBTC=0xb902b9899d8652219b8c62bc714020e2a0fa99e6
-seth send 0xf0d61329351932e6e83517e5d0cb666dacb9589d "createExchange(address)" 0xb902b9899d8652219b8c62bc714020e2a0fa99e6
-
-seth call 0xf0d61329351932e6e83517e5d0cb666dacb9589d "getExchange(address)" 0xb902b9899d8652219b8c62bc714020e2a0fa99e6 | xargs seth --abi-decode "x()(address)"
-
-# exchange:
-export EXCHANGE=0x5823ff557798ab14378c44ebf1fe0f327048de10
-export MINT_AMT=10000000
-
-seth send $TBTC "mint(address,uint256)" $ETH_FROM $MINT_AMT
-seth call $TBTC "balanceOf(address)" $ETH_FROM
-seth send $TBTC "approve(address,uint)" $EXCHANGE $MINT_AMT
-
-seth call $TBTC "allowance(address,address)" $ETH_FROM $EXCHANGE
-
-seth send --from $ETH_FROM --value 402343423423423 $EXCHANGE "addLiquidity(uint256,uint256,uint256)" 0 $MINT_AMT 1000000000000
-seth send --from $ETH_FROM --value 402343423423423 $EXCHANGE "addLiquidity(uint256,uint256,uint256)" 0 $MINT_AMT 100000000000000
-
-402343423423423
-100000000000000
-
-seth call $EXCHANGE "getTokenToEthInputPrice(uint)(uint)" 1
-0000000000000000000000000000000000000000000000000000000026218fd9
-0000000000000000000000000000000000000000000000000000000015770098
-
-
-export BUYER=$(seth accounts | sed -n 2p | awk '{ print $1 }')
-
-# function ethToTokenSwapInput(uint256 min_tokens, uint256 deadline) external payable returns (uint256  tokens_bought);
-seth send --from $BUYER --value 402343423423423 $EXCHANGE "ethToTokenSwapInput(uint256,uint256)" 3 1000000000000
-
-
-some issue with the block timestamp, probs encoded as hex
-
-
-0x9e9073f8d4e877d88da842a039a3698fb5267723 http://127.0.0.1:8545
-0x86841052ae15beb5a1b95148999ef9c640de7463 http://127.0.0.1:8545
-0x69a97fe59cfab7b9f8fd2854f99a895f00e1f5c6 http://127.0.0.1:8545
-0x1583e8a226c926ded00fa583c30ff8d8a6bc9452 http://127.0.0.1:8545
-
-
-exchange.methods.addLiquidity(
-    min_liquidity,
-    max_tokens,
-    deadline
-)
-
-
-The ethAmount sent to addLiquidity is the exact amount of ETH that will be deposited into the liquidity reserves. It should be 50% of the total value a liquidity provider wishes to deposit into the reserves.
-
-value = 10000
-
-the Uniswap smart contracts use ethAmount to determine the amount of ERC20 tokens that must be deposited. This token amount is the remaining 50% of total value a liquidity provider wishes to deposit.
-
-Since exchange rate can change between when a transaction is signed and when it is executed on Ethereum, max_tokens is used to bound the amount this rate can fluctuate.
-
-For the first liquidity provider, max_tokens is the exact amount of tokens deposited.
-
-Liquidity tokens are minted to track the relative proportion of total reserves that each liquidity provider has contributed.
-
-min_liquidity is used in combination with max_tokens and ethAmount to bound the rate at which liquidity tokens are minted.
-
-For the first liquidity provider, min_liquidity does not do anything and can be set to 0.
-
- */
