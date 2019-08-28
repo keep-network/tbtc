93f5b5015c80d2d5ee8b037199e9c4694d3a691c
liamzebedee <liamzebedee@yahoo.com.au>
2019-08-28 14:39:39 +0200

    Add Uniswap stubs for tests

---
+++ implementation/test/contracts/uniswap/UniswapExchangeStub.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.5.10;
+
+import {TBTCToken} from '../../../contracts/system/TBTCToken.sol';
+
+contract UniswapExchangeStub {
+    TBTCToken tbtc;
+
+    uint256 ethPrice = 10**8;
+    // uint256 tbtcPrice = 0;
+
+    constructor(address _tbtc) public {
+        tbtc = TBTCToken(_tbtc);
+    }
+
+    // Give some mock liquidity to the stub
+    // function mockLiquidity(uint256 _tbtcAmount) public payable {
+    //     require(msg.value > 0, "requires ETH for liquidity");
+    //     tbtc.transferFrom(msg.sender, address(this), _tbtcAmount);
+    // }
+
+    // function setPrices(uint256 _ethPrice, uint256 _tbtcPrice) public {
+    //     ethPrice = _ethPrice;
+    //     tbtcPrice = _tbtcPrice;
+    // }
+
+    function getTokenToEthInputPrice(uint256 tokens_sold)
+        external view
+        returns (uint256)
+    {
+        tokens_sold;
+        return ethPrice;
+    }
+
+    function ethToTokenSwapOutput(uint256 tokens_bought, uint256 deadline)
+        external payable
+        returns (uint256 eth_sold)
+    {
+        deadline;
+        // require(msg.value     == ethPrice, "incorrect eth sent");
+        // require(tokens_bought == tbtcPrice, "incorrect tbtc ask");
+        // require(tbtc.balanceOf(address(this)) >= tokens_bought, "not enough tbtc for trade");
+        tbtc.mint(msg.sender, tokens_bought);
+        // require(tbtc.transferFrom(address(this), msg.sender, tokens_bought), "transfer failed");
+        return msg.value;
+    }
+}
