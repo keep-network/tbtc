e40a52d608c54e3cae1637eb66ad7df935ea8828
Nicholas Evans <nicholas.evans@thesis.co>
2019-08-28 20:36:04 +0300

    Add more documentation to TestToken force functions

--- implementation/test/contracts/deposit/TestToken.sol
+++ implementation/test/contracts/deposit/TestToken.sol
@@ -12,26 +12,28 @@ contract TestToken is TBTCToken{
         // solium-disable-previous-line no-empty-blocks
     }
 
-    /// @dev             Mints an amount of the token and assigns it to an account.
-    ///                  Uses the internal _mint function
+    /// @dev             We Can't call TBTCToken mint function from deposit Test becuase of ACL.
+    ///                  This function bypasses ACL and can be called in Deposit tests
+    ///                  Mints an amount of the token and assigns it to an account.
+    ///                  Uses the internal _mint function.
     /// @param _account  The account that will receive the created tokens.
     /// @param _amount   The amount of tokens that will be created.
     function forceMint(address _account, uint256 _amount) public returns (bool){
         // NOTE: this is a public function with unchecked minting.
-        // TODO: enforce calling authority.
         _mint(_account, _amount);
         return true;
     }
 
-    /// @dev             Burns an amount of the token of a given account
+    /// @dev             We Can't call TBTCToken burn function from deposit Test becuase of ACL. 
+    ///                  This function bypasses ACL and can be called in Deposit tests.
+    ///                  Burns an amount of the token of a given account
     ///                  deducting from the sender's allowance for said account.
-    ///                  Uses the internal _burn function.
+    ///                  Bypasses ACL and can be called in Deposit tests
     /// @param _account  The account whose tokens will be burnt.
     /// @param _amount   The amount of tokens that will be burnt.
     function forceBurn(address _account, uint256 _amount) public {
         // NOTE: this uses internal function _burn instead of _burnFrom.
         // This will bypass allowance check for now.
-        // TODO: enforce calling authority.
         _burn(_account, _amount);
     }
 }
