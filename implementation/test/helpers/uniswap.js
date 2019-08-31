829bfab1945ca15e7c69d6b4c7c0304646f292ff
liamzebedee <liamzebedee@yahoo.com.au>
2019-08-31 11:52:27 +0200

    remove extraneous comment

--- implementation/test/helpers/uniswap.js
+++ implementation/test/helpers/uniswap.js
@@ -1,10 +1,6 @@
 const BN = require('bn.js')
 
 export class UniswapHelpers {
-  // see https://github.com/Uniswap/contracts-vyper/issues/27#issuecomment-476148467
-  // static GAS_ESTIMATE = {
-  // }
-
   static getDeadline() {
     const DEADLINE_FROM_NOW = 300 // TX expires in 300 seconds (5 minutes)
     const deadline = Math.ceil(Date.now() / 1000) + DEADLINE_FROM_NOW
