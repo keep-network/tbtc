b2c15866a4b5a7b543b06a719a70b4dbf5114835
liamzebedee <liamzebedee@yahoo.com.au>
2019-09-04 17:25:21 +0100

    Hack to make script run on macOS

--- implementation/scripts/migrate-externals.sh
+++ implementation/scripts/migrate-externals.sh
@@ -11,7 +11,10 @@ uniswap() {
     export ETH_RPC_URL="http://localhost:8545"
     UNISWAP_DEPLOYMENT=$(npm run migrate)
     ADDR=$(echo "$UNISWAP_DEPLOYMENT" | sed -n /Factory/p | cut -d' ' -f2)
-    sed -i '' -e "/UniswapFactoryAddress/s/0x[a-fA-F0-9]\{0,40\}/$ADDR/" ../../migrations/2_deploy_contracts.js
+    sed -i -e "/UniswapFactoryAddress/s/0x[a-fA-F0-9]\{0,40\}/$ADDR/" ../../migrations/2_deploy_contracts.js
+    if [[ -e ../../migrations/2_deploy_contracts.js-e ]]; then
+        rm ../../migrations/2_deploy_contracts.js-e
+    fi
 }
 
 uniswap
