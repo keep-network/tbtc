6490cafec1775257b68afd580ad3cfa13ef3c53d
liamzebedee <liamzebedee@yahoo.com.au>
2019-09-09 12:19:00 +0100

    Remove unused functions in Uniswap interfaces

--- implementation/contracts/external/IUniswapFactory.sol
+++ implementation/contracts/external/IUniswapFactory.sol
@@ -2,16 +2,9 @@ pragma solidity 0.5.10;
 
 /* solium-disable */
 
-contract IUniswapFactory {
-    // Public Variables
-    address public exchangeTemplate;
-    uint256 public tokenCount;
+interface IUniswapFactory {
     // Create Exchange
     function createExchange(address token) external returns (address exchange);
     // Get Exchange and Token Info
     function getExchange(address token) external view returns (address exchange);
-    function getToken(address exchange) external view returns (address token);
-    function getTokenWithId(uint256 tokenId) external view returns (address token);
-    // Never use
-    function initializeFactory(address template) external;
 }
