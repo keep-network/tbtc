a87d00ae8768bfe8f155ded93145ba395f27325e
liamzebedee <liamzebedee@yahoo.com.au>
2019-09-10 12:20:20 +0100

    Added skip/only to e2e helper, documentation

--- implementation/test/helpers/e2e.js
+++ implementation/test/helpers/e2e.js
@@ -1,7 +1,29 @@
+// A collection of E2E tests to run as part of a test suite
+export const e2eTests = []
+
+function e2eTest(runner, title, tests) {
+  const test = () => runner(title, tests)
+  e2eTests.push(test)
+}
 
 // eslint-disable-next-line no-only-tests/no-only-tests
 /**
  * A helper to structure end-to-end (E2E) integration tests.
- * Separates the unit test from the integration test suites.
+ * Tests run with this helper will be added to the integration test suite, and
+ * only run during `npm run test:e2e`.
+ *
+ * The API mirrors `describe`, so usage should look like:
+ * ```
+ * e2e('Title', function() {
+ *  it('behaviour')
+ * })
+ * ```
+ * @param {string} title
+ * @param {Function} tests
+ * @return {Suite}
  */
-export const e2e = process.env.TEST_E2E == 'true' ? contract.only : contract.skip
+const e2e = (title, tests) => e2eTest(contract, title, tests)
+e2e.skip = (title, tests) => e2eTest(contract.skip, title, tests)
+e2e.only = (title, tests) => e2eTest(contract.only, title, tests)
+
+export { e2e }
