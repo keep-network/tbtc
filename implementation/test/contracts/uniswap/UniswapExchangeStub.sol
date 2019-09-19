e5270160841869941a322471f6879e7f95efb650
liamzebedee <liamzebedee@yahoo.com.au>
2019-09-19 12:31:37 +0100

    Exchange stub implements IUniswapExchange

--- implementation/test/contracts/uniswap/UniswapExchangeStub.sol
+++ implementation/test/contracts/uniswap/UniswapExchangeStub.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.5.10;
 
 import {TestToken} from '../deposit/TestToken.sol';
+import {IUniswapExchange} from '../../../contracts/external/IUniswapExchange.sol';
 
-contract UniswapExchangeStub {
+contract UniswapExchangeStub is IUniswapExchange {
     TestToken tbtc;
-
 
     // The below returns an absurdly large price for tBTC
     // such that attemptToLiquidateOnchain will return early, from not being funded enough
@@ -16,6 +16,17 @@ contract UniswapExchangeStub {
 
     function setEthPrice(uint256 _ethPrice) public {
         ethPrice = _ethPrice;
+    }
+
+    function addLiquidity(uint256 min_liquidity, uint256 max_tokens, uint256 deadline)
+        external payable
+        returns (uint256)
+    {
+        require(msg.value > 0, "ETH missing from addLiquidity");
+        tbtc.forceMint(address(this), max_tokens);
+        // Stub doesn't implement the internal Uniswap token, UNI
+        // So return 0 here for total minted UNI.
+        return 0;
     }
 
     function getEthToTokenOutputPrice(uint256 tokens_sold)
@@ -31,8 +42,9 @@ contract UniswapExchangeStub {
         returns (uint256 eth_sold)
     {
         deadline;
-        require(msg.value     == ethPrice, "incorrect eth sent");
-        tbtc.forceMint(msg.sender, tokens_bought);
+        require(msg.value == ethPrice, "incorrect eth sent");
+        require(tbtc.balanceOf(address(this)) >= tokens_bought, "not enough TBTC liquidity mocked");
+        tbtc.transferFrom(address(this), msg.sender, tokens_bought);
         return msg.value;
     }
 }
