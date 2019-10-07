595b73e43eea515b98aefb7c254f07a96dce224e
Nicholas Evans <nicholas.evans@thesis.co>
2019-10-07 13:38:43 -0500

    Fix comment typo

--- implementation/test/contracts/deposit/TestTBTCConstants.sol
+++ implementation/test/contracts/deposit/TestTBTCConstants.sol
@@ -32,7 +32,7 @@ library TestTBTCConstants {
     uint256 public constant COURTESY_CALL_DURATION = 6 * 60 * 60; // seconds
     uint256 public constant AUCTION_DURATION = 24 * 60 * 60; // seconds
     uint256 public constant AUCTION_BASE_PERCENTAGE = 90; // percents
-    uint256 public constant PERMITTED_FEE_BUMPS = 5; // number of times the fee be increased 
+    uint256 public constant PERMITTED_FEE_BUMPS = 5; // number of times the fee can be increased 
 
     function getLotSize() public pure returns (uint256) { return MINIMUM_LOT_SIZE; }
     function getSignerFeeDivisor() public pure returns (uint256) { return SIGNER_FEE_DIVISOR; }
