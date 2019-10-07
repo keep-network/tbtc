558dc1f6b8ee62edfcfbe38b9ef46461327960cb
liamzebedee <liamzebedee@yahoo.com.au>
2019-10-07 11:58:36 +0100

    Automatically migrate + inject systems for demo
    This script acts on Keep tECDSA, tBTC, and tBTC maintainers. Run with `cd demo && ./testnet/build.sh`

---
+++ implementation/demo/testnet/build.sh
@@ -0,0 +1,63 @@
+# 
+# Deploys and sets up config for a demo testnet
+# 
+#!/bin/bash
+set -ex
+
+
+# Setup sed for macOS environments
+# 
+
+SED="sed"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if ! [ -x "$(command -v gsed)" ]; then
+        # See also: https://unix.stackexchange.com/questions/13711/differences-between-sed-on-mac-osx-and-other-standard-sed
+        echo 'Error: gsed is not installed.' >&2
+        echo 'GNU sed is required due to difference in functionality from OSX/FreeBSD sed.'
+        echo 'Install with: brew install gnu-sed'
+        exit 1
+    fi
+    SED="gsed"
+fi
+
+
+
+
+cd $GOPATH
+
+# --------------
+# 1. Keep tECDSA
+# --------------
+cd $GOPATH/src/github.com/keep-network/keep-tecdsa
+TECDSA_MIGRATION=$(truffle migrate --reset)
+
+# The sed regex needs to match both:
+# - Deploying KeepRegistry
+# - Replacing KeepRegistry
+# it then matches a range until the next Deploying, or the end of the migration log
+# Then it's a simple extraction of the contract address
+KEEP_REGISTRY=$(echo "$TECDSA_MIGRATION" | $SED -n "/'KeepRegistry'/,/total cost/p" | $SED -n "/contract address:/p" | awk '{ print $4 }')
+ECDSA_KEEP_FACTORY=$(echo "$TECDSA_MIGRATION" | $SED -n "/'ECDSAKeepFactory'/,/total cost/p" | $SED -n "/contract address:/p" | awk '{ print $4 }')
+$SED -i -e "/ECDSAKeepFactory = /s/0x[a-fA-F0-9]\{0,40\}/$ECDSA_KEEP_FACTORY/" configs/config.toml
+
+
+# -------
+# 2. tBTC
+# -------
+cd $GOPATH/src/github.com/keep-network/tbtc/implementation
+
+# Uncomment if you want to redeploy Uniswap (shouldn't be needed)
+# cd scripts
+# ./deploy_uniswap.sh
+# cd ..
+
+TBTC_MIGRATION=$(truffle migrate --reset)
+TBTC_SYSTEM=$(echo "$TBTC_MIGRATION" | $SED -n "/'TBTCSystem'/,/total cost/p" | $SED -n "/contract address:/p" | awk '{ print $4 }')
+
+
+# -------------------
+# 3. tBTC maintainers
+# -------------------
+cd $GOPATH/src/github.com/keep-network/tbtc-maintainers
+$SED -i -e "/TBTCSystem = /s/0x[a-fA-F0-9]\{0,40\}/$TBTC_SYSTEM/" configs/config.toml
