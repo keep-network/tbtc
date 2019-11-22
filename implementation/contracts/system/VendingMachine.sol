c6c14b39f5cf517f891ba7d3dd79a7f391fd178d
liamzebedee <liamzebedee@yahoo.com.au>
2019-11-22 19:22:40 +1000

    Fix dirty Sublime Text tabs+spaces orgy

--- implementation/contracts/system/VendingMachine.sol
+++ implementation/contracts/system/VendingMachine.sol
@@ -1,19 +1,19 @@
 contract VendingMachine {
-    function isQualified(deposit, proof) {
-        // check deposit qualified	
+	function isQualified(deposit, proof) {
+		// check deposit qualified	
 		totalValue = 0
-        unqualifiedDeposits = [...]
-        for deposit in unqualifiedDeposits:
-            totalValue += deposit.lotSize
+		unqualifiedDeposits = [...]
+		for deposit in unqualifiedDeposits:
+			totalValue += deposit.lotSize
 		
 		blockReward = 12.5
 		minBlocksX = totalValue / blockReward
 
 		proof = verifyProof(proof)
-        
-        // qualifier = 6 + n
-        // where 6 is the minimum number of confs to mint tbtc and
-        //       n is the security margin that fluctuates according to opened deposit volume
+		
+		// qualifier = 6 + n
+		// where 6 is the minimum number of confs to mint tbtc and
+		//       n is the security margin that fluctuates according to opened deposit volume
 		require(proof.blocks >= Math.min(6, minBlocksX));
-    }
+	}
 }
