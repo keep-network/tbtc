keep-network/tbtc

commit 7850e3313e7b2bf1a4e849abfd5cb12aae4e1217

Add DOT ACL to requestRedemption

implementation/contracts/deposit/Deposit.sol
@@ -36,11 +36,13 @@ contract Deposit {
     function createNewDeposit(
         address _TBTCSystem,
         address _TBTCToken,
+        address _DepositOwnerToken,
         uint256 _m,
         uint256 _n
     ) public payable returns (bool) {
         self.TBTCSystem = _TBTCSystem;
         self.TBTCToken = _TBTCToken;
+        self.DepositOwnerToken = _DepositOwnerToken;
         self.createNewDeposit(_m, _n);
         return true;
     }

implementation/contracts/deposit/DepositRedemption.sol
@@ -12,6 +12,7 @@ import {OutsourceDepositLogging} from "./OutsourceDepositLogging.sol";
 import {TBTCConstants} from "./TBTCConstants.sol";
 import {TBTCToken} from "../system/TBTCToken.sol";
 import {DepositLiquidation} from "./DepositLiquidation.sol";
+import {DepositOwnerToken} from "../system/DepositOwnerToken.sol";
 
 library DepositRedemption {
 
@@ -73,6 +74,10 @@ library DepositRedemption {
     ) public {
         require(_d.inRedeemableState(), "Redemption only available from Active or Courtesy state");
         require(_requesterPKH != bytes20(0), "cannot send value to zero pkh");
+        require(
+            msg.sender == DepositOwnerToken(_d.DepositOwnerToken).ownerOf(uint256(address(this))),
+            "redemption can only be called by deposit owner"
+        );
 
         redemptionTBTCBurn(_d);
 

implementation/contracts/deposit/DepositUtils.sol
@@ -25,6 +25,7 @@ library DepositUtils {
         // SET DURING CONSTRUCTION
         address TBTCSystem;
         address TBTCToken;
+        address DepositOwnerToken;
         uint8 currentState;
 
         // SET ON FRAUD

implementation/contracts/proxy/DepositFactory.sol
@@ -38,6 +38,7 @@ contract DepositFactory is CloneFactory{
     function createDeposit (
         address _TBTCSystem,
         address _TBTCToken,
+        address __DepositOwnerToken,
         uint256 _keepThreshold,
         uint256 _keepSize
     ) public payable returns(address) {
@@ -46,6 +47,7 @@ contract DepositFactory is CloneFactory{
         Deposit(address(uint160(cloneAddress))).createNewDeposit.value(msg.value)(
             _TBTCSystem,
             _TBTCToken,
+            __DepositOwnerToken,
             _keepThreshold,
             _keepSize);
 

commit 99fee9978f8f81e3cb3f2914bf2093187290bdd4

Fix calls to factory.createDeposit
These were broken after 7850e3313e7b2bf1a4e849abfd5cb12aae4e1217

implementation/test/DepositFactoryTest.js
@@ -50,6 +50,7 @@ contract('DepositFactory', (accounts) => {
       await factory.createDeposit(
         tbtcSystemStub.address,
         tbtcToken.address,
+        utils.address0,
         1,
         1,
         { value: funderBondAmount }
@@ -58,6 +59,7 @@ contract('DepositFactory', (accounts) => {
       await factory.createDeposit(
         tbtcSystemStub.address,
         tbtcToken.address,
+        utils.address0,
         1,
         1,
         { value: funderBondAmount }
@@ -77,6 +79,7 @@ contract('DepositFactory', (accounts) => {
       await factory.createDeposit(
         tbtcSystemStub.address,
         tbtcToken.address,
+        utils.address0,
         1,
         1,
         { value: funderBondAmount }
@@ -107,6 +110,7 @@ contract('DepositFactory', (accounts) => {
       await factory.createDeposit(
         tbtcSystemStub.address,
         tbtcToken.address,
+        utils.address0,
         1,
         1,
         { value: funderBondAmount }
@@ -115,6 +119,7 @@ contract('DepositFactory', (accounts) => {
       await factory.createDeposit(
         tbtcSystemStub.address,
         tbtcToken.address,
+        utils.address0,
         1,
         1,
         { value: funderBondAmount }
@@ -165,6 +170,7 @@ contract('DepositFactory', (accounts) => {
       await depositContract.createNewDeposit(
         tbtcSystemStub.address,
         tbtcToken.address,
+        utils.address0,
         1,
         1,
         { value: funderBondAmount }
@@ -184,6 +190,7 @@ contract('DepositFactory', (accounts) => {
       await factory.createDeposit(
         tbtcSystemStub.address,
         tbtcToken.address,
+        utils.address0,
         1,
         1,
         { value: funderBondAmount }

implementation/test/DepositFundingTest.js
@@ -112,6 +112,7 @@ contract('DepositFunding', (accounts) => {
       await testInstance.createNewDeposit(
         tbtcSystemStub.address,
         tbtcToken.address,
+        utils.address0,
         1, // m
         1,
         { value: funderBondAmount }
@@ -142,6 +143,7 @@ contract('DepositFunding', (accounts) => {
         testInstance.createNewDeposit.call(
           tbtcSystemStub.address,
           tbtcToken.address,
+          utils.address0,
           1, // m
           1),
         'Deposit setup already requested'

implementation/test/DepositUtilsTest.js
@@ -86,6 +86,7 @@ contract('DepositUtils', (accounts) => {
     await testUtilsInstance.createNewDeposit(
       tbtcSystemStub.address,
       tbtcToken.address,
+      utils.address0,
       1, // m
       1, // n
       { value: funderBondAmount }