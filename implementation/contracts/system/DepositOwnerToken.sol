18a96b1ba850eb10aee77da16e3bc07ab9c8a256
Nicholas Evans <nicholas.evans@thesis.co>
2019-11-29 01:49:53 -0600

    Fix sol:lint warning

--- implementation/contracts/system/DepositOwnerToken.sol
+++ implementation/contracts/system/DepositOwnerToken.sol
@@ -7,7 +7,7 @@ contract DepositOwnerToken is ERC721Metadata {
     constructor() ERC721Metadata("Deposit Owner Token", "DOT") public {
         // solium-disable-previous-line no-empty-blocks
     }
-     
+
     /// @dev Returns whether the specified token exists.
     /// @param tokenId uint256 ID of the token to query the existence of
     /// @return bool whether the token exists
