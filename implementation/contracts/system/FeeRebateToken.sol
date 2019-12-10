bfbcb099b922345a36632fb2dcfd89a2d67ff851
Nicholas Evans <nicholas.evans@thesis.co>
2019-12-10 03:18:36 -0600

    Implement Fee Rebate Token
    Decouple the token from TBTCSystem.
    Doesn'tm ake much sense to have the System contract behave as an ERC721

---
+++ implementation/contracts/system/FeeRebateToken.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.10;
+
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Metadata.sol";
+
+contract FeeRebateToken is ERC721Metadata {
+
+    constructor() ERC721Metadata("Fee Rebate Token", "FRT") public {
+        // solium-disable-previous-line no-empty-blocks
+    }
+
+    /// @dev Mints a new token.
+    /// Reverts if the given token ID already exists.
+    /// @param to The address that will own the minted token
+    /// @param tokenId uint256 ID of the token to be minted
+    function mint(address _to, uint256 _tokenId) public {
+        _mint(_to, _tokenId);
+    }
+
+    /// @dev Returns whether the specified token exists.
+    /// @param tokenId uint256 ID of the token to query the existence of
+    /// @return bool whether the token exists
+    function exists(uint256 tokenId) public view returns (bool) {
+        return _exists(tokenId);
+    }
+}
