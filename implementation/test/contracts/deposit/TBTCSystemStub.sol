192ef53426714f28a1298dfed8eb9d4f5bd8667d
Nicholas Evans <nicholas.evans@thesis.co>
2019-12-10 05:23:54 -0600

    Remove ERC721 functinality from test TBTCSystem

--- implementation/test/contracts/deposit/TBTCSystemStub.sol
+++ implementation/test/contracts/deposit/TBTCSystemStub.sol
@@ -5,14 +5,6 @@ import {TBTCSystem} from '../../../contracts/system/TBTCSystem.sol';
 contract TBTCSystemStub is TBTCSystem {
     address keepAddress = address(7);
 
-    constructor(address _depositFactory)
-        // Set expected factory address to 0-address.
-        // Address is irelevant as test use forceMint function to bypass ACL
-        TBTCSystem(_depositFactory)
-    public {
-        // solium-disable-previous-line no-empty-blocks
-    }
-
     function setOraclePrice(uint256 _oraclePrice) external {
         oraclePrice = _oraclePrice;
     }
@@ -33,15 +25,5 @@ contract TBTCSystemStub is TBTCSystem {
     function requestNewKeep(uint256 _m, uint256 _n) external payable returns (address _keepAddress) {
         _m; _n;
         return keepAddress;
-    }
-
-    /// @notice          Function to mint a new token.
-    /// @dev             We can't call 721 mint function from deposit Test becuase of ACL.
-    ///                  This function bypasses ACL and can be called in Deposit tests
-    ///                  Reverts if the given token ID already exists.
-    /// @param _to       The address that will own the minted token
-    /// @param _tokenId  uint256 ID of the token to be minted
-    function forceMint(address _to, uint256 _tokenId) public {
-        _mint(_to, _tokenId);
     }
 }
