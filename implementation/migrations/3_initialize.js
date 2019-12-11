c40945cdd1001d829e04e7b50134c5a147535da2
liamzebedee <liamzebedee@yahoo.com.au>
2019-12-11 14:49:10 +1000

    Remove old price "oracle" and replace with new one
    New price oracle relies on externally deployed medianizer price feeds, which as of now, aren't ready yet.
    
    This also renames uses of "oracle" to "feed"
    cc: the Prestwich
    https://github.com/keep-network/tbtc/pull/333

--- implementation/migrations/3_initialize.js
+++ implementation/migrations/3_initialize.js
@@ -1,14 +1,21 @@
 const TBTCSystem = artifacts.require('TBTCSystem')
+const BTCETHPriceFeed = artifacts.require('BTCETHPriceFeed')
 
 const {
   KeepRegistryAddress,
+  BTCUSDPriceFeed,
+  ETHUSDPriceFeed,
 } = require('./externals')
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network) {
   // Don't enact this setup during unit testing.
   if (process.env.NODE_ENV == 'test' && !process.env.INTEGRATION_TEST) return
 
   // system
   const tbtcSystem = await TBTCSystem.deployed()
   await tbtcSystem.initialize(KeepRegistryAddress)
+
+  // price feed
+  const btcEthPriceFeed = await BTCETHPriceFeed.deployed()
+  await btcEthPriceFeed.initialize(BTCUSDPriceFeed, ETHUSDPriceFeed)
 }
--- implementation/migrations/externals.js
+++ implementation/migrations/externals.js
@@ -1,6 +1,12 @@
 // Configuration for addresses of externally deployed smart contracts
 const KeepRegistryAddress = '0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
 
+// Medianized price feeds.
+const BTCUSDPriceFeed = '0xBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB'
+const ETHUSDPriceFeed = '0xCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC'
+
 module.exports = {
   KeepRegistryAddress,
+  BTCUSDPriceFeed,
+  ETHUSDPriceFeed,
 }
