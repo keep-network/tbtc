596aeca1d40ff2f0987d7589901c534eb0307406
NicholasDotSol <nicholas.evans@thesis.co>
2020-01-24 04:28:52 -0600

    fix sol:lint error

--- implementation/contracts/deposit/DepositFunding.sol
+++ implementation/contracts/deposit/DepositFunding.sol
@@ -58,7 +58,7 @@ library DepositFunding {
         require(_d.inStart(), "Deposit setup already requested");
         /* solium-disable-next-line value-in-payable */
         require(msg.value == TBTCConstants.getFunderBondAmount(), "incorrect funder bond amount");
-        require(_system.isAllowedLotSize(_lotSize), 'provided lot size not supported');
+        require(_system.isAllowedLotSize(_lotSize), "provided lot size not supported");
         // TODO: Whole value is stored as funder bond in the deposit, but part
         // of it should be transferred to keep: https://github.com/keep-network/tbtc/issues/297
         _d.lotSizeBtc = _lotSize;
