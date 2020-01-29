48f3a9168ce0cba182b39e359a968337229766f7
Antonio Salazar Cardozo <antonio@keep.network>
2020-01-29 13:46:39 -0500

    Fix linter issues for test/utils.deploySystem
    These were being ignored, and related to camel-casing and for loop
    usage. This is fixed.

--- implementation/test/utils.js
+++ implementation/test/utils.js
@@ -39,17 +39,15 @@ function chainToProofBytes(chain) {
   return byteString
 }
 
-// eslint-disable-next-line camelcase
-async function deploySystem(deploy_list) {
+async function deploySystem(deployList) {
   const deployed = {} // name: contract object
   const linkable = {} // name: linkable address
 
-  // eslint-disable-next-line camelcase,guard-for-in
-  for (const i in deploy_list) {
-    await deploy_list[i].contract.link(linkable)
-    const contract = await deploy_list[i].contract.new()
-    linkable[deploy_list[i].name] = contract.address
-    deployed[deploy_list[i].name] = contract
+  for (let i = 0; i < deployList.length; ++i) {
+    await deployList[i].contract.link(linkable)
+    const contract = await deployList[i].contract.new()
+    linkable[deployList[i].name] = contract.address
+    deployed[deployList[i].name] = contract
   }
   return deployed
 }
