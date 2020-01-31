7362b9a395f07d33e791505d60f844b4ea9774c6
NicholasDotSol <nicholas.evans@thesis.co>
2020-01-31 12:12:51 -0600

    remove _ prefix from internal address field

--- implementation/contracts/system/TBTCSystemAuthority.sol
+++ implementation/contracts/system/TBTCSystemAuthority.sol
@@ -2,16 +2,16 @@ pragma solidity 0.5.10;
 
 contract TBTCSystemAuthority {
 
-    address internal _tbtcSystem;
+    address internal tbtcSystem;
 
     /// @notice Set the address of the System contract on contract initialization
     constructor(address _tbtcSystem) public {
-        _tbtcSystem = _tbtcSystem;
+        tbtcSystem = _tbtcSystem;
     }
 
     /// @notice Function modifier ensures modified function is only called by set deeposit factory
     modifier onlyTbtcSystem(){
-        require(msg.sender == _tbtcSystem, "Caller must be depositFactory contract");
+        require(msg.sender == tbtcSystem, "Caller must be depositFactory contract");
         _;
     }
 }
