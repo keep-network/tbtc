da9f42170f8bdbb2c6e3fca1e48d1738478ea548
NicholasDotSol <nicholas.evans@thesis.co>
2020-01-31 11:35:31 -0600

    tbtcSystemAddress -> tbtcSystem

--- implementation/contracts/system/TBTCSystemAuthority.sol
+++ implementation/contracts/system/TBTCSystemAuthority.sol
@@ -5,13 +5,13 @@ contract TBTCSystemAuthority {
     address internal _tbtcSystem;
 
     /// @notice Set the address of the System contract on contract initialization
-    constructor(address _tbtcSystemAddress) public {
-        _tbtcSystem = _tbtcSystemAddress;
+    constructor(address _tbtcSystem) public {
+        _tbtcSystem = _tbtcSystem;
     }
 
     /// @notice Function modifier ensures modified function is only called by set deeposit factory
     modifier onlyTbtcSystem(){
-        require(msg.sender == _tbtcSystemAddress, "Caller must be depositFactory contract");
+        require(msg.sender == _tbtcSystem, "Caller must be depositFactory contract");
         _;
     }
 }
