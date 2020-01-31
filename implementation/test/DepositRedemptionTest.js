19c5fa5efc20d5433de20bfa699dc25c98039576
NicholasDotSol <nicholas.evans@thesis.co>
2020-01-31 03:19:36 -0600

    Fix performRedemptionTBTCTransfers test
    Bad test from approveandcall conflict :)

--- implementation/test/DepositRedemptionTest.js
+++ implementation/test/DepositRedemptionTest.js
@@ -264,10 +264,9 @@ contract('DepositRedemption', (accounts) => {
       await restoreSnapshot()
     })
 
-    it('does nothing if deposit is pre-term and requester is FRT holder', async () => {
-      const requesterAddress = accounts[0]
-      await testInstance.setRequestInfo(requesterAddress, '0x' + '11'.repeat(20), 0, 0, '0x' + '22'.repeat(32))
-      await feeRebateToken.transferFrom(accounts[4], requesterAddress, tdtId, { from: accounts[4] })
+    it('does nothing if deposit is pre-term, redeemer is FRT holder and signerFee is escrowed', async () => {
+      await tbtcToken.forceMint(testInstance.address, signerFee)
+      await feeRebateToken.transferFrom(accounts[4], accounts[0], tdtId, { from: accounts[4] })
 
       block = await web3.eth.getBlock('latest')
       await testInstance.performRedemptionTBTCTransfers()
