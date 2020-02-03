d3c4c6526b5040a47a52f43f678bd04ef7bcd20d
Matt Luongo <mhluongo@gmail.com>
2020-02-02 21:55:24 -0500

    Allow the owner to set deposit collateral factors
    Restrict the initial collateral factor to <= 300%, lest this function
    become an accidental killswitch. While this decision is better for
    system resilience, it means less flexibility around changing market
    conditions.

--- implementation/contracts/system/TBTCSystem.sol
+++ implementation/contracts/system/TBTCSystem.sol
@@ -21,6 +21,7 @@ contract TBTCSystem is Ownable, ITBTCSystem, DepositLog {
     event AllowNewDepositsUpdated(bool _allowNewDeposits);
     event SignerFeeDivisorUpdated(uint256 _signerFeeDivisor);
     event CollateralizationThresholdsUpdated(
+        uint256 _initialCollateralizedPercent,
         uint256 _undercollateralizedThresholdPercent,
         uint256 _severelyUndercollateralizedThresholdPercent
     );
@@ -38,7 +39,8 @@ contract TBTCSystem is Ownable, ITBTCSystem, DepositLog {
     // Parameters governed by the TBTCSystem owner
     bool private allowNewDeposits = false;
     uint256 private signerFeeDivisor = 200; // 1/200 == 50bps == 0.5% == 0.005
-    uint128 private undercollateralizedThresholdPercent = 140;  // percent
+    uint128 private initialCollateralizedPercent = 150;  // percent
+    uint128 private undercollateralizedThresholdPercent = 135;  // percent
     uint128 private severelyUndercollateralizedThresholdPercent = 120; // percent
     uint256[] lotSizesSatoshis = [10**5, 10**6, 10**7, 20**7, 50**7, 10**8]; // [0.001, 0.01, 0.1, 0.2, 0.5, 1.0] BTC
 
@@ -82,8 +84,8 @@ contract TBTCSystem is Ownable, ITBTCSystem, DepositLog {
     /// @notice gets whether new deposits are allowed
     function getAllowNewDeposits() external view returns (bool) { return allowNewDeposits; }
 
-    /// @notice One-time-use emergency function to disallow future deposit creation for 10 days. 
-    function emergencyPauseNewDeposits() external onlyOwner returns (bool) { 
+    /// @notice One-time-use emergency function to disallow future deposit creation for 10 days.
+    function emergencyPauseNewDeposits() external onlyOwner returns (bool) {
         require(pausedTimestamp == 0, "emergencyPauseNewDeposits can only be called once");
         pausedTimestamp = block.timestamp;
         allowNewDeposits = false;
@@ -121,7 +123,7 @@ contract TBTCSystem is Ownable, ITBTCSystem, DepositLog {
     function getSignerFeeDivisor() external view returns (uint256) { return signerFeeDivisor; }
 
     /// @notice Set the allowed deposit lot sizes.
-    /// @dev    Lot sizes should be 
+    /// @dev    Lot sizes should be
     /// @param _lotSizes Array of allowed lot sizes.
     function setLotSizes(uint256[] calldata _lotSizes) external onlyOwner {
         for( uint i = 0; i < _lotSizes.length; i++){
@@ -135,14 +137,14 @@ contract TBTCSystem is Ownable, ITBTCSystem, DepositLog {
     }
 
     /// @notice Gets the allowed lot sizes
-    /// @return Uint256 array of allowed lot sizes 
+    /// @return Uint256 array of allowed lot sizes
     function getAllowedLotSizes() external view returns (uint256[] memory){
         return lotSizesSatoshis;
     }
 
     /// @notice Check if a lot size is allowed.
     /// @param _lotSize Lot size to check.
-    /// @return True if lot size is allowed, false otherwise. 
+    /// @return True if lot size is allowed, false otherwise.
     function isAllowedLotSize(uint256 _lotSize) external view returns (bool){
         for( uint i = 0; i < lotSizesSatoshis.length; i++){
             if (lotSizesSatoshis[i] == _lotSize){
@@ -153,19 +155,31 @@ contract TBTCSystem is Ownable, ITBTCSystem, DepositLog {
     }
 
     /// @notice Set the system collateralization levels
+    /// @param _initialCollateralizedPercent default signing bond percent for new deposits
     /// @param _undercollateralizedThresholdPercent first undercollateralization trigger
     /// @param _severelyUndercollateralizedThresholdPercent second undercollateralization trigger
     function setCollateralizationThresholds(
+        uint128 _initialCollateralizedPercent,
         uint128 _undercollateralizedThresholdPercent,
         uint128 _severelyUndercollateralizedThresholdPercent
     ) external onlyOwner {
         require(
-            _undercollateralizedThresholdPercent > _severelyUndercollateralizedThresholdPercent,
-            "Severe undercollateralized threshold must be > undercollateralized threshold"
+            _initialCollateralizedPercent <= 300,
+            "Initial collateralized percent must be <= 300%"
         );
+        require(
+            _initialCollateralizedPercent > _undercollateralizedThresholdPercent,
+            "Undercollateralized threshold must be < initial collateralized percent"
+        );
+        require(
+            _undercollateralizedThresholdPercent > _severelyUndercollateralizedThresholdPercent,
+            "Severe undercollateralized threshold must be < undercollateralized threshold"
+        );
+        initialCollateralizedPercent = _initialCollateralizedPercent;
         undercollateralizedThresholdPercent = _undercollateralizedThresholdPercent;
         severelyUndercollateralizedThresholdPercent = _severelyUndercollateralizedThresholdPercent;
         emit CollateralizationThresholdsUpdated(
+            _initialCollateralizedThresholdPercent,
             _undercollateralizedThresholdPercent,
             _severelyUndercollateralizedThresholdPercent
         );
