c079ab7957bb165ec1f9fce8e58cd4e8308612d2
Antonio Salazar Cardozo <antonio@keep.network>
2020-02-20 21:47:19 -0500

    prettier pass of JavaScript code
    It is time.

--- implementation/test/TBTCSystemTest.js
+++ implementation/test/TBTCSystemTest.js
@@ -1,109 +1,127 @@
-const { deployAndLinkAll } = require('../testHelpers/testDeployer.js')
-const { increaseTime } = require('../testHelpers/utils.js')
-const { createSnapshot, restoreSnapshot } = require('../testHelpers/helpers/snapshot.js')
-const { accounts, contract, web3 } = require('@openzeppelin/test-environment')
-const { BN, expectRevert } = require('@openzeppelin/test-helpers')
-const { expect } = require('chai')
+const {deployAndLinkAll} = require("../testHelpers/testDeployer.js")
+const {increaseTime} = require("../testHelpers/utils.js")
+const {
+  createSnapshot,
+  restoreSnapshot,
+} = require("../testHelpers/helpers/snapshot.js")
+const {accounts, contract, web3} = require("@openzeppelin/test-environment")
+const {BN, expectRevert} = require("@openzeppelin/test-helpers")
+const {expect} = require("chai")
 
-const TBTCSystem = contract.fromArtifact('TBTCSystem')
+const TBTCSystem = contract.fromArtifact("TBTCSystem")
 
-describe('TBTCSystem', async function() {
+describe("TBTCSystem", async function() {
   let tbtcSystem
   let ecdsaKeepFactory
 
   before(async () => {
-    const {
-      tbtcSystemStub,
-      ecdsaKeepFactoryStub,
-    } = await deployAndLinkAll(
+    const {tbtcSystemStub, ecdsaKeepFactoryStub} = await deployAndLinkAll(
       [],
       // Though deployTestDeposit deploys a TBTCSystemStub for us, we want to
       // test TBTCSystem itself.
-      { TBTCSystemStub: TBTCSystem }
+      {TBTCSystemStub: TBTCSystem},
     )
     // Refer to this correctly throughout the rest of the test.
     tbtcSystem = tbtcSystemStub
     ecdsaKeepFactory = ecdsaKeepFactoryStub
   })
 
-  describe('requestNewKeep()', async () => {
+  describe("requestNewKeep()", async () => {
     let openKeepFee
     before(async () => {
       openKeepFee = await ecdsaKeepFactory.openKeepFeeEstimate.call()
     })
-    it('sends caller as owner to open new keep', async () => {
+    it("sends caller as owner to open new keep", async () => {
       const expectedKeepOwner = accounts[2]
 
-      await tbtcSystem.requestNewKeep(5, 10, 0, { from: expectedKeepOwner, value: openKeepFee })
+      await tbtcSystem.requestNewKeep(5, 10, 0, {
+        from: expectedKeepOwner,
+        value: openKeepFee,
+      })
       const keepOwner = await ecdsaKeepFactory.keepOwner.call()
 
-      expect(expectedKeepOwner, 'incorrect keep owner address').to.equal(keepOwner)
+      expect(expectedKeepOwner, "incorrect keep owner address").to.equal(
+        keepOwner,
+      )
     })
 
-    it('returns keep address', async () => {
+    it("returns keep address", async () => {
       const expectedKeepAddress = await ecdsaKeepFactory.keepAddress.call()
 
-      const result = await tbtcSystem.requestNewKeep.call(5, 10, 0, { value: openKeepFee })
+      const result = await tbtcSystem.requestNewKeep.call(5, 10, 0, {
+        value: openKeepFee,
+      })
 
-      expect(expectedKeepAddress, 'incorrect keep address').to.equal(result)
+      expect(expectedKeepAddress, "incorrect keep address").to.equal(result)
     })
 
-    it('forwards value to keep factory', async () => {
+    it("forwards value to keep factory", async () => {
       const initialBalance = await web3.eth.getBalance(ecdsaKeepFactory.address)
 
-      await tbtcSystem.requestNewKeep(5, 10, 0, { value: openKeepFee })
+      await tbtcSystem.requestNewKeep(5, 10, 0, {value: openKeepFee})
 
       const finalBalance = await web3.eth.getBalance(ecdsaKeepFactory.address)
       const balanceCheck = new BN(finalBalance).sub(new BN(initialBalance))
-      expect(balanceCheck, 'TBTCSystem did not correctly forward value to keep factory').to.eq.BN(openKeepFee)
+      expect(
+        balanceCheck,
+        "TBTCSystem did not correctly forward value to keep factory",
+      ).to.eq.BN(openKeepFee)
     })
   })
 
-  describe('setSignerFeeDivisor', async () => {
-    it('sets the signer fee', async () => {
-      await tbtcSystem.setSignerFeeDivisor(new BN('201'))
+  describe("setSignerFeeDivisor", async () => {
+    it("sets the signer fee", async () => {
+      await tbtcSystem.setSignerFeeDivisor(new BN("201"))
 
       const signerFeeDivisor = await tbtcSystem.getSignerFeeDivisor()
-      expect(signerFeeDivisor).to.eq.BN(new BN('201'))
+      expect(signerFeeDivisor).to.eq.BN(new BN("201"))
     })
 
-    it('reverts if msg.sender != owner', async () => {
+    it("reverts if msg.sender != owner", async () => {
       await expectRevert.unspecified(
-        tbtcSystem.setSignerFeeDivisor(new BN('201'), { from: accounts[1] }),
-        ''
+        tbtcSystem.setSignerFeeDivisor(new BN("201"), {
+          from: accounts[1],
+        }),
+        "",
       )
     })
   })
 
-  describe('setLotSizes', async () => {
-    it('sets a different lot size array', async () => {
-      const blockNumber = await web3.eth.getBlock('latest').number
-      const lotSizes = [10**8, 10**6]
+  describe("setLotSizes", async () => {
+    it("sets a different lot size array", async () => {
+      const blockNumber = await web3.eth.getBlock("latest").number
+      const lotSizes = [10 ** 8, 10 ** 6]
       await tbtcSystem.setLotSizes(lotSizes)
 
-      const eventList = await tbtcSystem.getPastEvents('LotSizesUpdated', { fromBlock: blockNumber, toBlock: 'latest' })
+      const eventList = await tbtcSystem.getPastEvents("LotSizesUpdated", {
+        fromBlock: blockNumber,
+        toBlock: "latest",
+      })
       expect(eventList.length).to.equal(1)
-      expect(eventList[0].returnValues._lotSizes).to.eql(['100000000', '1000000']) // deep equality check
+      expect(eventList[0].returnValues._lotSizes).to.eql([
+        "100000000",
+        "1000000",
+      ]) // deep equality check
     })
 
-    it('reverts if lot size array is empty', async () => {
+    it("reverts if lot size array is empty", async () => {
       const lotSizes = []
       await expectRevert(
         tbtcSystem.setLotSizes(lotSizes),
-        'Lot size array must always contain 1BTC'
+        "Lot size array must always contain 1BTC",
       )
     })
 
-    it('reverts if lot size array does not contain a 1BTC lot size', async () => {
-      const lotSizes = [10**7]
+    it("reverts if lot size array does not contain a 1BTC lot size", async () => {
+      const lotSizes = [10 ** 7]
       await expectRevert(
         tbtcSystem.setLotSizes(lotSizes),
-        'Lot size array must always contain 1BTC'
+        "Lot size array must always contain 1BTC",
       )
     })
   })
 
-  describe('emergencyPauseNewDeposits', async () => {
+  describe("emergencyPauseNewDeposits", async () => {
     let term
 
     beforeEach(async () => {
@@ -114,21 +132,21 @@ describe('TBTCSystem', async function() {
       await restoreSnapshot()
     })
 
-    it('pauses new deposit creation', async () => {
+    it("pauses new deposit creation", async () => {
       await tbtcSystem.emergencyPauseNewDeposits()
 
       const allowNewDeposits = await tbtcSystem.getAllowNewDeposits()
       expect(allowNewDeposits).to.equal(false)
     })
 
-    it('reverts if msg.sender is not owner', async () => {
+    it("reverts if msg.sender is not owner", async () => {
       await expectRevert(
-        tbtcSystem.emergencyPauseNewDeposits({ from: accounts[1] }),
-        'Ownable: caller is not the owner'
+        tbtcSystem.emergencyPauseNewDeposits({from: accounts[1]}),
+        "Ownable: caller is not the owner",
       )
     })
 
-    it('does not allows new deposit re-activation before 10 days', async () => {
+    it("does not allows new deposit re-activation before 10 days", async () => {
       await tbtcSystem.emergencyPauseNewDeposits()
       term = await tbtcSystem.getRemainingPauseTerm()
 
@@ -136,11 +154,11 @@ describe('TBTCSystem', async function() {
 
       await expectRevert(
         tbtcSystem.resumeNewDeposits(),
-        'Deposits are still paused'
+        "Deposits are still paused",
       )
     })
 
-    it('allows new deposit creation after 10 days', async () => {
+    it("allows new deposit creation after 10 days", async () => {
       await tbtcSystem.emergencyPauseNewDeposits()
       term = await tbtcSystem.getRemainingPauseTerm()
 
@@ -150,7 +168,7 @@ describe('TBTCSystem', async function() {
       expect(allowNewDeposits).to.equal(true)
     })
 
-    it('reverts if emergencyPauseNewDeposits has already been called', async () => {
+    it("reverts if emergencyPauseNewDeposits has already been called", async () => {
       await tbtcSystem.emergencyPauseNewDeposits()
       term = await tbtcSystem.getRemainingPauseTerm()
 
@@ -159,7 +177,7 @@ describe('TBTCSystem', async function() {
 
       await expectRevert(
         tbtcSystem.emergencyPauseNewDeposits(),
-        'emergencyPauseNewDeposits can only be called once'
+        "emergencyPauseNewDeposits can only be called once",
       )
     })
   })
