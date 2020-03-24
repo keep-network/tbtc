c092693c6a0b6b8ce56476ce9dde1e93808728b2
Jakub Nowakowski <jakub.nowakowski@keep.network>
2020-03-24 10:01:10 +0100

    Revert "Implemented ECDSAKeepStub functions"
    This reverts commit fdb896a0a326e2aa41f4241baa9e01bdd90ab25b.

--- implementation/contracts/test/keep/ECDSAKeepStub.sol
+++ implementation/contracts/test/keep/ECDSAKeepStub.sol
@@ -4,17 +4,11 @@ import {
     IBondedECDSAKeep
 } from "@keep-network/keep-ecdsa/contracts/api/IBondedECDSAKeep.sol";
 
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-
 /// @notice Implementation of ECDSAKeep interface used in tests only
 /// @dev This is a stub used in tests, so we don't have to call actual ECDSAKeep
 contract ECDSAKeepStub is IBondedECDSAKeep {
-    using SafeMath for uint256;
-
     bytes publicKey;
     bool success;
-    uint256 memberCount = 3;
-    bool isActive = true;
     uint256 bondAmount = 10000;
 
     // Notification that the keep was requested to sign a digest.
@@ -33,34 +27,26 @@ contract ECDSAKeepStub is IBondedECDSAKeep {
         return bondAmount;
     }
 
-    function sign(bytes32 _digest) external onlyWhenActive {
+    function sign(bytes32 _digest) external {
         emit SignatureRequested(_digest);
     }
 
     function distributeETHReward() external payable {
-        uint256 dividend = msg.value.div(memberCount);
-
-        require(dividend > 0, "Dividend value must be non-zero");
+        // solium-disable-previous-line no-empty-blocks
     }
 
     function distributeERC20Reward(address _asset, uint256 _value) external {
-        uint256 dividend = _value.div(memberCount);
-
-        require(dividend > 0, "Dividend value must be non-zero");
+        // solium-disable-previous-line no-empty-blocks
     }
 
-    function seizeSignerBonds() external onlyWhenActive {
-        isActive = false;
-
+    function seizeSignerBonds() external {
         if (address(this).balance > 0) {
             msg.sender.transfer(address(this).balance);
         }
     }
 
     function returnPartialSignerBonds() external payable {
-        uint256 bondPerMember = msg.value.div(memberCount);
-
-        require(bondPerMember > 0, "Partial signer bond must be non-zero");
+        // solium-disable-previous-line no-empty-blocks
     }
 
     function submitSignatureFraud(
@@ -75,21 +61,11 @@ contract ECDSAKeepStub is IBondedECDSAKeep {
         return success;
     }
 
-    function closeKeep() external onlyWhenActive {
-        isActive = false;
-    }
-
-    modifier onlyWhenActive() {
-        require(isActive, "Keep is not active");
-        _;
+    function closeKeep() external {
+        // solium-disable-previous-line no-empty-blocks
     }
 
     // Functions to set data for tests.
-
-    function reset() public {
-        success = true;
-        isActive = true;
-    }
 
     function setPublicKey(bytes memory _publicKey) public {
         publicKey = _publicKey;
@@ -97,10 +73,6 @@ contract ECDSAKeepStub is IBondedECDSAKeep {
 
     function setSuccess(bool _success) public {
         success = _success;
-    }
-
-    function setMemberCount(uint256 _memberCount) public {
-        memberCount = _memberCount;
     }
 
     function setBondAmount(uint256 _bondAmount) public {
