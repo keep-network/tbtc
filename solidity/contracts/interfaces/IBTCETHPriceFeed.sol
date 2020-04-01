962eab772046fdfc576f7b990f206ace03533105
NicholasDotSol <nicholas.evans@thesis.co>
2020-03-31 22:13:45 -0500

    Add feed additino interface functions
    BTCETHPriceFeed Should implement the functions needed to
    add price feeds in case one breaks.

--- solidity/contracts/interfaces/IBTCETHPriceFeed.sol
+++ solidity/contracts/interfaces/IBTCETHPriceFeed.sol
@@ -1,8 +1,15 @@
 pragma solidity ^0.5.10;
+import "../external/IMedianizer.sol";
 
 interface IBTCETHPriceFeed {
     /// @notice Get the current price of bitcoin in ether.
     /// @dev This does not account for any 'Flippening' event.
     /// @return The price of one satoshi in wei.
     function getPrice() external view returns (uint256);
+
+    /// @notice add a new BTC/USD meidanizer to the internal btcUsdFeeds array
+    function addBtcUsdFeed(IMedianizer _btcUsdFeed) external;
+
+    /// @notice add a new ETH/USD meidanizer to the internal ethUsdFeeds array
+    function addEthUsdFeed(IMedianizer _ethUsdFeed) external;
 }
