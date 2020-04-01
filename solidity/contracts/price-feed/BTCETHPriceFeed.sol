67bab0f62505634eaa6c4bcebd5afaf71abde37f
NicholasDotSol <nicholas.evans@thesis.co>
2020-03-31 22:19:05 -0500

    Allow for btc/usd and eth/usd price feed addition
    Instead of holding one pair of feeds, hold an array of feeds for each (btcUsdFeeds and ethUsdFeeds)
    
    When calculating price, look for the first active price feed
    from each array and use that one for to get
    the corresponding asset price.
    
    Only `TBTCSystem` is allowed to add a new Medianizer feed

--- solidity/contracts/price-feed/BTCETHPriceFeed.sol
+++ solidity/contracts/price-feed/BTCETHPriceFeed.sol
@@ -11,9 +11,10 @@ contract BTCETHPriceFeed is Ownable, IBTCETHPriceFeed {
     using SafeMath for uint256;
 
     bool private _initialized = false;
+    address internal tbtcSystemAddress;
 
-    IMedianizer btcPriceFeed;
-    IMedianizer ethPriceFeed;
+    IMedianizer[] private btcUsdFeeds;
+    IMedianizer[] private ethUsdFeeds;
 
     constructor() public {
     // solium-disable-previous-line no-empty-blocks
@@ -23,15 +24,18 @@ contract BTCETHPriceFeed is Ownable, IBTCETHPriceFeed {
     /// @param _BTCUSDPriceFeed The BTC/USD price feed address.
     /// @param _ETHUSDPriceFeed The ETH/USD price feed address.
     function initialize(
-        address _BTCUSDPriceFeed,
-        address _ETHUSDPriceFeed
+        address _tbtcSystemAddress,
+        IMedianizer _BTCUSDPriceFeed,
+        IMedianizer _ETHUSDPriceFeed
     )
         external onlyOwner
     {
         require(!_initialized, "Already initialized.");
-
-        btcPriceFeed = IMedianizer(_BTCUSDPriceFeed);
-        ethPriceFeed = IMedianizer(_ETHUSDPriceFeed);
+        tbtcSystemAddress = _tbtcSystemAddress;
+        btcUsdFeeds.push(_BTCUSDPriceFeed);
+        ethUsdFeeds.push(_ETHUSDPriceFeed);
+        // btcPriceFeed = IMedianizer(_BTCUSDPriceFeed);
+        // ethPriceFeed = IMedianizer(_ETHUSDPriceFeed);
         _initialized = true;
     }
 
@@ -43,8 +47,7 @@ contract BTCETHPriceFeed is Ownable, IBTCETHPriceFeed {
     {
         // We typecast down to uint128, because the first 128 bits of
         // the medianizer oracle value is unrelated to the price.
-        uint256 btcUsd = uint256(uint128(btcPriceFeed.read()));
-        uint256 ethUsd = uint256(uint128(ethPriceFeed.read()));
+        (uint256 btcUsd, uint256 ethUsd) = _getComponentPrices();
         // The price is a ratio of bitcoin to ether is expressed as:
         //  x btc : y eth
         // Bitcoin has 10 decimal places, ether has 18. Normalising the units, we have:
@@ -53,5 +56,84 @@ contract BTCETHPriceFeed is Ownable, IBTCETHPriceFeed {
         //  x : y * 10^10
         // Due to order-of-ops, we can move the multiplication to get some more precision.
         return btcUsd.mul(10**10).div(ethUsd);
+    }
+
+    /// @notice Get the first active Medianizer contract from the btcUsdFeeds array.
+    /// @return The address of the first Active Medianizer. address(0) if none found
+    function getWorkingBtcUsdFeed() external view returns (address){
+        bool btcUsdActive;
+
+        for(uint i = 0; i < btcUsdFeeds.length; i++){
+            (, btcUsdActive) = btcUsdFeeds[i].peek();
+            if(btcUsdActive) {
+                return address(btcUsdFeeds[i]);
+            }
+        }
+        return address(0);
+    }
+
+    /// @notice Get the first active Medianizer contract from the ethUsdFeeds array.
+    /// @return The address of the first Active Medianizer. address(0) if none found
+    function getWorkingEthUsdFeed() external view returns (address){
+        bool ethUsdActive;
+
+        for(uint i = 0; i < ethUsdFeeds.length; i++){
+            (, ethUsdActive) = ethUsdFeeds[i].peek();
+            if(ethUsdActive) {
+                return address(ethUsdFeeds[i]);
+            }
+        }
+        return address(0);
+    }
+
+    /// @notice Add _btcUsdFeed to internal btcUsdFeeds array.
+    /// @dev IMedianizer must be active in order to add.
+    function addBtcUsdFeed(IMedianizer _btcUsdFeed) external onlyTbtcSystem {
+        bool btcUsdActive;
+        (, btcUsdActive) = _btcUsdFeed.peek();
+        require(btcUsdActive, "Cannot add inactive feed");
+        btcUsdFeeds.push(_btcUsdFeed);
+    }
+
+    /// @notice Add _ethUsdFeed to internal btcUsdFeeds array.
+    /// @dev IMedianizer must be active in order to add.
+    function addEthUsdFeed(IMedianizer _ethUsdFeed) external onlyTbtcSystem {
+        bool ethUsdActive;
+        (, ethUsdActive) = _ethUsdFeed.peek();
+        require(ethUsdActive, "Cannot add inactive feed");
+        ethUsdFeeds.push(_ethUsdFeed);
+    }
+
+    /// @notice Get the current value of BTCUSD and ETHUSD price feeds.
+    /// @dev sequentially traverses the `btcUsdFeeds` and `ethUsdFeeds` arrays
+    /// and will revert if no price feed is active from either set.
+    /// @return The BTCUSD and ETHUSD price feed values.
+    function _getComponentPrices()
+        internal view returns (uint256 btcUsd, uint256 ethUsd)
+    {
+        bool btcUsdActive;
+        bool ethUsdActive;
+
+        for(uint i = 0; i < btcUsdFeeds.length; i++){
+            (btcUsd, btcUsdActive) = btcUsdFeeds[i].peek();
+            if(btcUsdActive) {
+                break;
+            }
+        }
+
+        for(uint i = 0; i < ethUsdFeeds.length; i++){
+            (ethUsd, ethUsdActive) = ethUsdFeeds[i].peek();
+            if(ethUsdActive) {
+                break;
+            }
+        }
+
+        require(btcUsdActive && ethUsdActive, "Price feed offline");
+    }
+
+    /// @notice Function modifier ensures modified function is only called by tbtcSystemAddress.
+    modifier onlyTbtcSystem(){
+        require(msg.sender == tbtcSystemAddress, "Caller must be tbtcSystem contract");
+        _;
     }
 }
