cb04ed3a9b0e757b7156fd29b324c4d9b8addb7b
Nicholas Evans <nicholas.evans@thesis.co>
2020-05-04 13:10:24 -0500

    Deposit fallback remove restriction
    Remove restriction for msg.sender on fallback.
    Signer bonds enter the system through a bonding contract,
    not the backing ecdsaKeep address.
    
    Note that removing this also allows anyone to send ether to the Deposit.
    Ether sent to this contract by accident will not be retreivable.

--- solidity/contracts/deposit/Deposit.sol
+++ solidity/contracts/deposit/Deposit.sol
@@ -36,7 +36,6 @@ contract Deposit is DepositFactoryAuthority {
     }
 
     function () external payable {
-        require(msg.sender == self.keepAddress, "Deposit contract can only receive ETH from underlying keep");
     }
 
     /// @notice     Get the keep contract address associated with the Deposit.
