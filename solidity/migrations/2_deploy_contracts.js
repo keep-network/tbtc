346c36ba81c98492e727de94e0e2a360e8004432
Sloan Thompson <sloan.thompson@thesis.co>
2020-05-06 19:41:36 -0400

    Pre-commits are messed up I guess, linting for test

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -24,7 +24,9 @@ const SatWeiPriceFeed = artifacts.require("SatWeiPriceFeed")
 
 // Bitcoin difficulty relays.
 const Relay = artifacts.require("@summa-tx/relay-sol/contracts/Relay")
-const TestnetRelay = artifacts.require("@summa-tx/relay-sol/contracts/TestnetRelay")
+const TestnetRelay = artifacts.require(
+  "@summa-tx/relay-sol/contracts/TestnetRelay",
+)
 const MockRelay = artifacts.require("MockRelay")
 
 // system
@@ -69,15 +71,15 @@ const bitcoinMain = {
   height: 629070,
   epochStart:
     "0x459ec50d4ea62a89da04eb1ef3e352ec740bca50e8a808000000000000000000",
-};
+}
 
 const bitcoinTest = {
   genesis:
     "0x0000c0205d1103efc13e6647977e3d65f253c3e762451e9ca9b920517d000000000000008442a07bcde3292a888277ea6337ba5bbdfa808ae01535846f19d843144c8f60478bb15e7b41011a88be5d36",
   height: 1723030,
   epochStart:
-    "0xe2657f702faa9470815005305c45b4be2271c22ade1348e6fe00000000000000"
-};
+    "0xe2657f702faa9470815005305c45b4be2271c22ade1348e6fe00000000000000",
+}
 
 module.exports = (deployer, network, accounts) => {
   deployer.then(async () => {
