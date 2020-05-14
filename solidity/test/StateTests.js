b1d77d2bbf439303cdd2254a4133164a7c477283
Nicholas Evans <nicholas.evans@thesis.co>
2020-05-14 08:34:23 -0500

    Add new runStatePath for courtesyCall transition

--- solidity/test/StateTests.js
+++ solidity/test/StateTests.js
@@ -3,12 +3,26 @@ const {deployAndLinkAll} = require("./helpers/fullDeployer.js")
 const {runStatePath} = require("./states/run.js")
 
 describe("tBTC states", () => {
-  runStatePath(
-    states,
-    deployAndLinkAll(),
-    "start",
-    "awaitingSignerSetup",
-    "awaitingFundingProof",
-    "active",
-  )
+  describe("simple redeem", () => {
+    runStatePath(
+      states,
+      deployAndLinkAll(),
+      "start",
+      "awaitingSignerSetup",
+      "awaitingFundingProof",
+      "active",
+      "awaitingWithdrawalSignature",
+    )
+  })
+  describe("Courtesy-Active", () => {
+    runStatePath(
+      states,
+      deployAndLinkAll(),
+      "start",
+      "awaitingSignerSetup",
+      "awaitingFundingProof",
+      "active",
+      "courtesyCall",
+    )
+  })
 })
