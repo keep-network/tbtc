4c3d89412c5290104f14a38693397584d2a27259
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-05-27 16:28:12 +0200

    Externalize relay genesis config
    Relay genesis blocks should be taken
    from a separate json file. It will be
    easier to provide those values
    from the outside.

--- solidity/migrations/2_deploy_contracts.js
+++ solidity/migrations/2_deploy_contracts.js
@@ -23,6 +23,8 @@ const prices = require("./prices")
 const SatWeiPriceFeed = artifacts.require("SatWeiPriceFeed")
 
 // Bitcoin difficulty relays.
+const relayGenesis = require("./relay-genesis.json")
+
 const OnDemandSPV = artifacts.require(
   "@summa-tx/relay-sol/contracts/OnDemandSPV",
 )
@@ -67,22 +69,6 @@ const all = [
   VendingMachine,
   FeeRebateToken,
 ]
-
-const bitcoinMain = {
-  genesis:
-    "0x00006020dd02d03c03dbc1f41312a6940e89919ce67fbf99a20307000000000000000000260d70e7ae07c80db07fbf29d09ec1a86d4f788e58098189a6f9021236572a7dd99eb15e397a11178294a823",
-  height: 629070,
-  epochStart:
-    "0x459ec50d4ea62a89da04eb1ef3e352ec740bca50e8a808000000000000000000",
-}
-
-const bitcoinTest = {
-  genesis:
-    "0x0000c0205d1103efc13e6647977e3d65f253c3e762451e9ca9b920517d000000000000008442a07bcde3292a888277ea6337ba5bbdfa808ae01535846f19d843144c8f60478bb15e7b41011a88be5d36",
-  height: 1723030,
-  epochStart:
-    "0xe2657f702faa9470815005305c45b4be2271c22ade1348e6fe00000000000000",
-}
 
 module.exports = (deployer, network, accounts) => {
   deployer.then(async () => {
@@ -132,12 +118,12 @@ module.exports = (deployer, network, accounts) => {
     // see https://github.com/summa-tx/relays . On testnet, we use a local
     // mock.
     if (network === "mainnet") {
-      const {genesis, height, epochStart} = bitcoinMain
+      const {genesis, height, epochStart} = relayGenesis.bitcoinMain
 
       await deployer.deploy(OnDemandSPV, genesis, height, epochStart, 0)
       difficultyRelay = await OnDemandSPV.deployed()
     } else if (network == "keep_dev" || network == "ropsten") {
-      const {genesis, height, epochStart} = bitcoinTest
+      const {genesis, height, epochStart} = relayGenesis.bitcoinTest
 
       await deployer.deploy(TestnetRelay, genesis, height, epochStart, 0)
       difficultyRelay = await TestnetRelay.deployed()
