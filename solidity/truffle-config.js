04f8ba45f68f50587c5cab00596336ac82594ee8
Sloan Thompson <sloan.thompson@thesis.co>
2020-06-03 17:07:30 -0400

    Read ETH host from env var

--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -67,7 +67,7 @@ module.exports = {
 
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(process.env.CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY, "https://ropsten.infura.io/v3/59fb36a36fa4474b890c13dd30038be5")
+        return new HDWalletProvider(process.env.CONTRACT_OWNER_ETH_ACCOUNT_PRIVATE_KEY, process.env.ETH_HOSTNAME)
       },
       gas: 6721975,
       network_id: 3
