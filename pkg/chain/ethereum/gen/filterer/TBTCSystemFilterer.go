aa3467ea58a62b45a199b38c82d0844b409bacc2
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-10-23 13:27:01 +0200

    Add TBTCSystem events filterer
    Added a `TBTCSystemFilterer` allowing
    to access past events emitted by the
    `TBTCSystem` contract. This is a temporary
    workaround because contract bindings generated by
    the `keep-common` generator don't
    support this feature yet.

---
+++ pkg/chain/ethereum/gen/filterer/TBTCSystemFilterer.go
@@ -0,0 +1,79 @@
+package filterer
+
+import (
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	abi "github.com/keep-network/tbtc/pkg/chain/ethereum/gen/abi/system"
+	"math/big"
+)
+
+// FIXME: This is a temporary structure allowing to access past events
+//  emitted by the `TBTCSystemFilterer` contract. This structure is
+//  here because the generated contract wrappers from `gen/contract`
+//  don't support `Filter*` methods yet. When the contract generator
+//  will support those methods, the below structure can be removed.
+type TBTCSystemFilterer struct {
+	contract *abi.TBTCSystem
+}
+
+func NewTBTCSystemFilterer(
+	contractAddress common.Address,
+	backend bind.ContractBackend,
+) (*TBTCSystemFilterer, error) {
+	contract, err := abi.NewTBTCSystem(contractAddress, backend)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TBTCSystemFilterer{contract}, nil
+}
+
+type TBTCSystemRedemptionRequested struct {
+	DepositContractAddress common.Address
+	Requester              common.Address
+	Digest                 [32]byte
+	UtxoValue              *big.Int
+	RedeemerOutputScript   []byte
+	RequestedFee           *big.Int
+	Outpoint               []byte
+}
+
+func (tsf *TBTCSystemFilterer) FilterRedemptionRequested(
+	depositsAddresses []common.Address,
+	startBlock uint64,
+	endBlock *uint64,
+) ([]*TBTCSystemRedemptionRequested, error) {
+	iterator, err := tsf.contract.FilterRedemptionRequested(
+		&bind.FilterOpts{
+			Start: startBlock,
+			End:   endBlock,
+		},
+		depositsAddresses,
+		nil,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	events := make([]*TBTCSystemRedemptionRequested, 0)
+
+	for {
+		if !iterator.Next() {
+			break
+		}
+
+		event := iterator.Event
+		events = append(events, &TBTCSystemRedemptionRequested{
+			DepositContractAddress: event.DepositContractAddress,
+			Requester:              event.Requester,
+			Digest:                 event.Digest,
+			UtxoValue:              event.UtxoValue,
+			RedeemerOutputScript:   event.RedeemerOutputScript,
+			RequestedFee:           event.RequestedFee,
+			Outpoint:               event.Outpoint,
+		})
+	}
+
+	return events, nil
+}
