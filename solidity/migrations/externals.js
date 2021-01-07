3b18fd2eeccdad5c2e81679f4579012c77438145
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-01-07 20:14:26 +0100

    Hardcode address of difficulty relay for ropsten
    On ropsten we use TestnetRelay that is already deployed and maintainer
    for this relay is working and catching up latest blocks. If we deploy
    fresh contract for ropsten we would need to catch up all the blocks
    since configured genesis (which is block 1775996).

--- solidity/migrations/externals.js
+++ solidity/migrations/externals.js
@@ -7,10 +7,13 @@ const BondedECDSAKeepFactoryAddress = "0xZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 // See: https://github.com/makerdao/oracles-v2#live-mainnet-oracles
 const ETHBTCMedianizer = "0xABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE"
 
+// Ropsten contracts.
 const RopstenETHBTCPriceFeed = "0xe9046e086137d2c0ffe60035391f6d7b4ec16733"
+const RopstenTestnetRelay = "0xcF3a6246879aab9eb7beC0d936743208EA51d0Ed"
 
 module.exports = {
   BondedECDSAKeepFactoryAddress,
   ETHBTCMedianizer,
   RopstenETHBTCPriceFeed,
+  RopstenTestnetRelay,
 }
