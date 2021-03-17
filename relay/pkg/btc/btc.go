97da6f239316602c7bff1e6e847254494fdee5a5
Tomasz Slabon <tomasz.slabon@keep.network>
2021-03-17 20:29:02 +0100

    Btc connectivity: implemented function that provides relay header for a block at given height

--- relay/pkg/btc/btc.go
+++ relay/pkg/btc/btc.go
@@ -1,22 +1,35 @@
 package btc
 
 import (
+	"encoding/hex"
+	"fmt"
 	"math/big"
 )
 
 // Handle represents a handle to the Bitcoin chain.
 type Handle interface {
 	// GetHeaderByHeight returns the block header for the given block height.
-	GetHeaderByHeight(height *big.Int) *Header
+	GetHeaderByHeight(height *big.Int) (*Header, error)
 }
 
 // Header represents a Bitcoin block header.
 type Header struct {
-	Raw        []byte
 	Hash       [32]byte
-	Height     uint64
+	Height     int64
 	Prevhash   [32]byte
 	MerkleRoot [32]byte
+	Raw        []byte
+}
+
+func (h *Header) String() string {
+	return fmt.Sprintf(
+		"Hash: %s, Height: %d, PrevHash: %s, MerkleRoot: %s, Raw: %s",
+		hex.EncodeToString(h.Hash[:]),
+		h.Height,
+		hex.EncodeToString(h.Prevhash[:]),
+		hex.EncodeToString(h.MerkleRoot[:]),
+		hex.EncodeToString(h.Raw),
+	)
 }
 
 type Config struct {
--- relay/pkg/btc/remote/remote.go
+++ relay/pkg/btc/remote/remote.go
@@ -1,10 +1,13 @@
 package remote
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"math/big"
 
 	"github.com/btcsuite/btcd/rpcclient"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/ipfs/go-log"
 	"github.com/keep-network/tbtc/relay/pkg/btc"
 )
@@ -14,6 +17,18 @@ var logger = log.Logger("relay-btc-remote")
 // remoteChain represents a remote Bitcoin chain.
 type remoteChain struct {
 	rpcClient *rpcclient.Client
+}
+
+func serializeHeader(header *wire.BlockHeader) ([]byte, error) {
+	var buffer bytes.Buffer
+	writer := io.Writer(&buffer)
+	err := header.Serialize(writer)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return buffer.Bytes(), nil
 }
 
 // Connect connects to the Bitcoin chain and returns a chain handle.
@@ -30,10 +45,14 @@ func Connect(config *btc.Config) (btc.Handle, error) {
 
 	if err != nil {
 		return nil, fmt.Errorf(
-			"failed to connect to Bitcoin node at [%s]: [%v]",
-			config.URL,
-			err,
-		)
+			"failed to create rpc client at [%s]: [%v]", config.URL, err)
+	}
+
+	// TODO: Try to find a better way to test connection
+	_, err = client.GetBlockCount()
+	if err != nil {
+		return nil, fmt.Errorf(
+			"rpc client failed to connect at [%s]: [%v]", config.URL, err)
 	}
 
 	// TODO: Remember to shutdown client
@@ -41,6 +60,39 @@ func Connect(config *btc.Config) (btc.Handle, error) {
 }
 
 // GetHeaderByHeight returns the block header for the given block height.
-func (rc *remoteChain) GetHeaderByHeight(height *big.Int) *btc.Header {
-	return nil
+func (rc *remoteChain) GetHeaderByHeight(height *big.Int) (*btc.Header, error) {
+	blockHash, err := rc.rpcClient.GetBlockHash(height.Int64())
+
+	if err != nil {
+		return nil, fmt.Errorf(
+			"getblockhash failed for height [%d]: [%v]", height.Int64(), err)
+	}
+
+	blockHeader, err := rc.rpcClient.GetBlockHeader(blockHash)
+
+	if err != nil {
+		return nil, fmt.Errorf(
+			"getblockheader failed for block with hash [%s]: [%v]",
+			blockHash.String(),
+			err)
+	}
+
+	rawHeader, err := serializeHeader(blockHeader)
+
+	if err != nil {
+		return nil, fmt.Errorf(
+			"failed to serialize header for block with hash [%s]: [%v]",
+			blockHash.String(),
+			err)
+	}
+
+	relayHeader := btc.Header{
+		Hash:       blockHeader.BlockHash(),
+		Prevhash:   blockHeader.PrevBlock,
+		MerkleRoot: blockHeader.MerkleRoot,
+		Raw:        rawHeader,
+		Height:     height.Int64(),
+	}
+
+	return &relayHeader, nil
 }
