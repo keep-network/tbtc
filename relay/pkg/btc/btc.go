41ba031b3d502fc1298806d4972884d846bab338
Tomasz Slabon <tomasz.slabon@keep.network>
2021-03-19 14:42:34 +0100

    Added description of fields in relay Header

--- relay/pkg/btc/btc.go
+++ relay/pkg/btc/btc.go
@@ -14,11 +14,17 @@ type Handle interface {
 
 // Header represents a Bitcoin block header.
 type Header struct {
-	Hash       [32]byte
-	Height     int64
-	PrevHash   [32]byte
+	// Hash is the hash of the block
+	Hash [32]byte
+	// Height is the height of the block in the blockchain
+	Height int64
+	// PrevHash is the hash of the previous block
+	PrevHash [32]byte
+	// MerkleRoot is the hash of the root of the Merkle tree of transcations in
+	// the block
 	MerkleRoot [32]byte
-	Raw        []byte
+	// Raw is the serialized data of the block header; 80-bytes long
+	Raw []byte
 }
 
 func (h *Header) String() string {
