299034e34deb237c5ba3a8e0125b3dc9da6838bc
Jakub Nowakowski <jakub.nowakowski@keep.network>
2021-05-05 16:25:06 +0200

    Use npm links for inter module dependencies
    For local installation we use npm links for dependencies between
    projects.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e pipefail
 
-# Dafault inputs.
-KEEP_ECDSA_PATH_DEFAULT=$(realpath -m $(dirname $0)/../../keep-ecdsa)
-
-# Read user inputs.
-read -p "Enter path to the keep-ecdsa project [$KEEP_ECDSA_PATH_DEFAULT]: " keep_ecdsa_path
-KEEP_ECDSA_PATH=$(realpath ${keep_ecdsa_path:-$KEEP_ECDSA_PATH_DEFAULT})
-
 # Run script.
 LOG_START='\n\e[1;36m'  # new line + bold + cyan
 LOG_END='\n\e[0m'       # new line + reset
@@ -17,31 +10,23 @@ DONE_END='\n\n\e[0m'    # new line + reset
 printf "${LOG_START}Starting installation...${LOG_END}"
 TBTC_PATH=$(realpath $(dirname $0)/../)
 TBTC_SOL_PATH=$(realpath $TBTC_PATH/solidity)
-KEEP_ECDSA_SOL_PATH=$(realpath $KEEP_ECDSA_PATH/solidity)
-KEEP_ECDSA_SOL_ARTIFACTS_PATH=$(realpath $KEEP_ECDSA_SOL_PATH/build/contracts)
 
 cd $TBTC_SOL_PATH
 
 printf "${LOG_START}Installing NPM dependencies...${LOG_END}"
 npm ci
+npm link @keep-network/keep-ecdsa
 
 printf "${LOG_START}Unlocking ethereum accounts...${LOG_END}"
 KEEP_ETHEREUM_PASSWORD=$KEEP_ETHEREUM_PASSWORD \
     npx truffle exec scripts/unlock-eth-accounts.js --network development
 
-printf "${LOG_START}Finding current ethereum network ID...${LOG_END}"
-
-output=$(npx truffle exec ./scripts/get-network-id.js --network development)
-NETWORKID=$(echo "$output" | tail -1)
-printf "Current network ID: ${NETWORKID}\n"
-
-printf "${LOG_START}Fetching external contracts addresses...${LOG_END}"
-KEEP_ECDSA_SOL_ARTIFACTS_PATH=$KEEP_ECDSA_SOL_ARTIFACTS_PATH \
-NETWORKID=$NETWORKID \
-    ./scripts/lcl-provision-external-contracts.sh
-
 printf "${LOG_START}Migrating contracts...${LOG_END}"
 npm run clean
 npx truffle migrate --reset --network development
+
+printf "${LOG_START}Creating links...${LOG_END}"
+ln -sf build/contracts artifacts
+npm link
 
 printf "${DONE_START}Installation completed!${DONE_END}"
