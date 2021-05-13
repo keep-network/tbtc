bfad4d7fb8e5e552d78ee4961da65d29cf1f855c
Beau Shinkle <beaushinkle@gmail.com>
2021-05-13 16:21:12 -0400

    Add string formatting

--- solidity/scripts/multiply-local-eth-btc-price.js
+++ solidity/scripts/multiply-local-eth-btc-price.js
@@ -4,17 +4,17 @@ async function run() {
   try {
     if (process.argv.length <= 4 || process.argv[4] === 'help') {
       console.log(
-`
-truffle exec multiply-local-eth-btc-price.js <factor>
-  Updates the local mock ETHBTC price by the specified factor.
-
-  <factor>
-    A factor to multiply the current ETHBTC price by. ETHBTC price
-    is in wei per satoshi, so a factor of 2 means the price of one satoshi
-    has doubled in terms of wei, i.e. it takes twice as many wei to buy
-    one satoshi. In terms of collateralization, this means a deposit that
-    was 100% collateralized is now 50% collateralized.
-`
+        `
+        |truffle exec multiply-local-eth-btc-price.js <factor>
+        |  Updates the local mock ETHBTC price by the specified factor.
+        |
+        |  <factor>
+        |    A factor to multiply the current ETHBTC price by. ETHBTC price
+        |    is in wei per satoshi, so a factor of 2 means the price of one satoshi
+        |    has doubled in terms of wei, i.e. it takes twice as many wei to buy
+        |    one satoshi. In terms of collateralization, this means a deposit that
+        |    was 100% collateralized is now 50% collateralized.
+        |`.replaceAll(/[ \t]+\|/g, '').trim()
       )
     } else if (isNaN(parseFloat(process.argv[4]))) {
       console.log('<factor> must be a number.')
