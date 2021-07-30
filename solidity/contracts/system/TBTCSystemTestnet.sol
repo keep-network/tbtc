5e65bdc43b328b01095e3c283ee48a7daef10a21
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-07-30 14:07:56 +0200

    Allow for 0.001 BTC lot size for testnet

---
+++ solidity/contracts/system/TBTCSystemTestnet.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.5.17;
+
+import {TBTCSystem} from "./TBTCSystem.sol";
+
+contract TBTCSystemTestnet is TBTCSystem {
+    constructor(address _priceFeed, address _relay)
+    TBTCSystem(_priceFeed, _relay)
+    public {
+        // solium-disable-previous-line no-empty-blocks
+        lotSizesSatoshis = [10**5, 10**6, 10**7, 2 * 10**7, 5 * 10**7, 10**8]; // [0.001, 0.01, 0.1, 0.2, 0.5, 1.0] BTC
+    }
+}
