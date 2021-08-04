f86ac7eaad4e0c13c218329caafc2fdc38fd0774
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-08-04 12:00:52 +0200

    Allow 10^3 and 10^4 lot sizes as well

--- solidity/contracts/system/TBTCSystemTestnet.sol
+++ solidity/contracts/system/TBTCSystemTestnet.sol
@@ -7,6 +7,6 @@ contract TBTCSystemTestnet is TBTCSystem {
     TBTCSystem(_priceFeed, _relay)
     public {
         // solium-disable-previous-line no-empty-blocks
-        lotSizesSatoshis = [10**5, 10**6, 10**7, 2 * 10**7, 5 * 10**7, 10**8]; // [0.001, 0.01, 0.1, 0.2, 0.5, 1.0] BTC
+        lotSizesSatoshis = [10**3, 10**4, 10**5, 10**6, 10**7, 2 * 10**7, 5 * 10**7, 10**8]; // [0.00001, 0.0001, 0.001, 0.01, 0.1, 0.2, 0.5, 1.0] BTC
     }
 }
