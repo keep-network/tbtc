154cb50a16875cd6e5825e45b702e5f62ec9d2ed
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2021-09-01 11:25:24 +0200

    Use fresh relay contract
    Fresh relay contract has been deployed because the
    previous one stayed far away the chain tip after
    some problems with the Ethereum provider. This
    change will allow to make e2e tests without
    waiting for relay maintainer to be synced.

--- solidity/migrations/externals.js
+++ solidity/migrations/externals.js
@@ -1,11 +1,11 @@
 // Medianized price feeds.
 // These are deployed and maintained by Maker.
 // See: https://github.com/makerdao/oracles-v2#live-mainnet-oracles
-const ETHBTCMedianizer = "0xABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE"
+const ETHBTCMedianizer = '0xABCDEABCDEABCDEABCDEABCDEABCDEABCDEABCDE'
 
 // Ropsten contracts.
-const RopstenETHBTCPriceFeed = "0xe9046e086137d2c0ffe60035391f6d7b4ec16733"
-const RopstenTestnetRelay = "0xcf3910EDa57722Ed712D9f9b4D783EaEdbD6534C"
+const RopstenETHBTCPriceFeed = '0xe9046e086137d2c0ffe60035391f6d7b4ec16733'
+const RopstenTestnetRelay = '0x7406a59e123a12830A2AFC7d7e8491C66766d8D3'
 
 module.exports = {
   ETHBTCMedianizer,
